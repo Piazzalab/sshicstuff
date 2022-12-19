d5ae61966a8c3c86e8cc2ab41b00779cf703613d
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-12-19 13:33:20 +0100

    pooled mean_df and pooled std_df

--- src/hic_ssdna/aggregate_centro.py
+++ src/hic_ssdna/aggregate_centro.py
@@ -140,6 +140,37 @@ def compute_aggregate_stats(df_centros_bins: pd.DataFrame,
     return df_mean, df_std, df_median
 
 
+def pooled_stats(mean_df: pd.DataFrame,
+                 std_df: pd.DataFrame):
+
+    middle = int(np.where(mean_df.index.values == 0)[0])
+    pooled_index = mean_df.index[middle:].values
+
+    #   Pool the mean dataframe
+    left_mean_df = mean_df.iloc[:middle+1]
+    left_mean_df.index = pooled_index[::-1]
+    left_mean_df = left_mean_df.sort_index()
+    right_mean_df = mean_df.iloc[middle:]
+
+    tmp_mean_df = pd.concat((left_mean_df, right_mean_df))
+    pooled_mean_df = tmp_mean_df.groupby(tmp_mean_df.index).mean()
+
+    #   Pool the std dataframe
+    left_std_df = std_df.iloc[:middle + 1]
+    left_std_df.index = pooled_index[::-1]
+    left_std_df = left_std_df.sort_index()
+    right_std_df = std_df.iloc[middle:]
+    pooled_std_df = pd.DataFrame()
+
+    for col in left_std_df.columns:
+        n1 = left_std_df[col].shape[0]
+        n2 = right_std_df[col].shape[0]
+        std_pooled = np.sqrt(((n1 - 1) * left_std_df[col] ** 2 + (n2 - 1) * right_std_df[col] ** 2) / (n1 + n2 - 2))
+        pooled_std_df[col] = std_pooled
+
+    return pooled_mean_df, pooled_std_df
+
+
 def plot_aggregated(mean_df: pd.DataFrame,
                     std_df: pd.DataFrame,
                     info_df: pd.DataFrame,
@@ -149,14 +180,17 @@ def plot_aggregated(mean_df: pd.DataFrame,
     made around the centromeres (average on the 16 chr of yeast).
     Gives also the standard deviation.
     """
-    x = mean_df.index.tolist()
-    for ii, oligo in enumerate(mean_df.columns):
+
+    pooled_mean_df, pooled_std_df = pooled_stats(mean_df=mean_df, std_df=std_df)
+
+    x = pooled_mean_df.index.tolist()
+    for ii, oligo in enumerate(pooled_mean_df.columns):
         probe = info_df.loc['names', oligo]
         if len(probe.split('-/-')) > 1:
             probe = '_&_'.join(probe.split('-/-'))
 
-        y = mean_df[oligo]
-        yerr = std_df[oligo]
+        y = pooled_mean_df[oligo]
+        yerr = pooled_std_df[oligo]
         plt.figure(figsize=(18, 12))
         plt.bar(x, y)
         plt.errorbar(x, y, yerr=yerr, fmt="o", color='b', capsize=5)
