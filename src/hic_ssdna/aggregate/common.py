9851820913fbf40fe5658bd99a19e2ff9030ed62
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-12-23 15:15:43 +0100

    rewrote aggregated plot

--- src/hic_ssdna/aggregate/common.py
+++ src/hic_ssdna/aggregate/common.py
@@ -45,75 +45,33 @@ def pooled_stats(mean_df: pd.DataFrame,
     return pooled_mean_df, pooled_std_df
 
 
-def plot_aggregated(mean_df: pd.DataFrame,
-                    std_df: pd.DataFrame,
-                    info_df: pd.DataFrame,
-                    mode: str,
-                    output_path: str,
-                    pooled: bool = True,
-                    limit: bool = False):
-    """
-    Plot for each oligo/read a barplot of the average number of contacts
-    made around the centromeres (average on the 16 chr of yeast).
-    Gives also the standard deviation.
-    """
+def plot_aggregated(
+        aggregated: dict[str: pd.DataFrame],
+        mode: str,
+        output_path: str,
+        pooled: bool = True):
 
-    # for ii, oligo in enumerate(mean_df.columns):
-    #     probe = info_df.loc['names', oligo]
-    #     if len(probe.split('-/-')) > 1:
-    #         probe = '_&_'.join(probe.split('-/-'))
-    #
-    #     means = mean_df[oligo]  # Mean Data
-    #     stds = std_df[oligo]  # Standard deviation Data
-    #     peakval = [str(m) for m in means]  # String array of means
-    #
-    #     ind = np.arange(len(means))
-    #     width = 0.35
-    #
-    #     plt.figure()
-    #     plt.title('Average Age')
-    #     plt.bar(ind, means, width, color='b', align='center', yerr=stds, ecolor='k')
-    #     plt.ylabel('Age (years)')
-    #
-    #     def autolabel(bars, peakval):
-    #         for ii, bar in enumerate(bars):
-    #             height = bars[ii]
-    #             plt.text(ind[ii], height - 5, '%s' % (peakval[ii]), ha='center', va='bottom')
-    #
-    #     autolabel(means, peakval)
+    for probe, df in aggregated.items():
+        mean = df.T.mean()
+        std = df.T.std()
 
-    if pooled:
-        mean_df, std_df = pooled_stats(mean_df=mean_df, std_df=std_df)
+        if pooled:
+            mean, std = pooled_stats(mean_df=pd.DataFrame(mean), std_df=pd.DataFrame(std))
+            mean = mean.squeeze()
+            std = std.squeeze()
 
-    if limit:
-        df_min = mean_df - std_df
-        df_max = mean_df + std_df
-        y_min = np.min(df_min.values) + 0.05 * np.min(df_min.values)
-        y_max = np.max(df_max.values) + 0.05 * np.max(df_max.values)
-    else:
-        y_min = -0.001
-        y_max = None
-
-    x = mean_df.index.tolist()
-    for ii, oligo in enumerate(mean_df.columns):
-        probe = info_df.loc['names', oligo]
-        if len(probe.split('-/-')) > 1:
-            probe = '_&_'.join(probe.split('-/-'))
-
-        y = mean_df[oligo]
-        yerr = std_df[oligo]
+        pos = mean.index
         plt.figure(figsize=(18, 12))
-        plt.bar(x, y)
-        plt.errorbar(x, y, yerr=yerr, fmt="o", color='b', capsize=5, uplims=False, lolims=False)
-        plt.ylim((y_min, y_max))
+        plt.bar(pos, mean)
+        plt.errorbar(pos, mean, yerr=std, fmt="o", color='b', capsize=5, clip_on=True)
+        plt.ylim((-0.0001, None))
         plt.title(
-            "Aggregated frequencies for read {0} from probe {1} around {2}".format(oligo, probe, mode))
+            "Aggregated frequencies for probe {0} around {1}".format(probe, mode))
         plt.xlabel("Bins around the centromeres (in kb), 5' to 3'")
         plt.xticks(rotation=45)
         plt.ylabel("Average frequency made and standard deviation")
         plt.savefig(output_path + "{0}-centromeres-aggregated_frequencies_plot.{1}".format(probe, 'jpg'), dpi=99)
         plt.close()
-
 
 def mkdir(output_path: str,
           mode: str):
