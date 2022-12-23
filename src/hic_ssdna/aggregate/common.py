604dcc7d6c83ee9ffb58d227bf8d1789e2f4765e
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2022-12-23 12:12:48 +0100

    WIP - aggregated plot function to change

--- src/hic_ssdna/aggregate/common.py
+++ src/hic_ssdna/aggregate/common.py
@@ -58,15 +58,41 @@ def plot_aggregated(mean_df: pd.DataFrame,
     Gives also the standard deviation.
     """
 
+    # for ii, oligo in enumerate(mean_df.columns):
+    #     probe = info_df.loc['names', oligo]
+    #     if len(probe.split('-/-')) > 1:
+    #         probe = '_&_'.join(probe.split('-/-'))
+    #
+    #     means = mean_df[oligo]  # Mean Data
+    #     stds = std_df[oligo]  # Standard deviation Data
+    #     peakval = [str(m) for m in means]  # String array of means
+    #
+    #     ind = np.arange(len(means))
+    #     width = 0.35
+    #
+    #     plt.figure()
+    #     plt.title('Average Age')
+    #     plt.bar(ind, means, width, color='b', align='center', yerr=stds, ecolor='k')
+    #     plt.ylabel('Age (years)')
+    #
+    #     def autolabel(bars, peakval):
+    #         for ii, bar in enumerate(bars):
+    #             height = bars[ii]
+    #             plt.text(ind[ii], height - 5, '%s' % (peakval[ii]), ha='center', va='bottom')
+    #
+    #     autolabel(means, peakval)
+
     if pooled:
         mean_df, std_df = pooled_stats(mean_df=mean_df, std_df=std_df)
 
-    y_min, y_max = (0, 0)
     if limit:
         df_min = mean_df - std_df
         df_max = mean_df + std_df
         y_min = np.min(df_min.values) + 0.05 * np.min(df_min.values)
         y_max = np.max(df_max.values) + 0.05 * np.max(df_max.values)
+    else:
+        y_min = -0.001
+        y_max = None
 
     x = mean_df.index.tolist()
     for ii, oligo in enumerate(mean_df.columns):
@@ -78,12 +104,12 @@ def plot_aggregated(mean_df: pd.DataFrame,
         yerr = std_df[oligo]
         plt.figure(figsize=(18, 12))
         plt.bar(x, y)
-        plt.errorbar(x, y, yerr=yerr, fmt="o", color='b', capsize=5)
-        if limit:
-            plt.ylim((y_min, y_max))
+        plt.errorbar(x, y, yerr=yerr, fmt="o", color='b', capsize=5, uplims=False, lolims=False)
+        plt.ylim((y_min, y_max))
         plt.title(
             "Aggregated frequencies for read {0} from probe {1} around {2}".format(oligo, probe, mode))
         plt.xlabel("Bins around the centromeres (in kb), 5' to 3'")
+        plt.xticks(rotation=45)
         plt.ylabel("Average frequency made and standard deviation")
         plt.savefig(output_path + "{0}-centromeres-aggregated_frequencies_plot.{1}".format(probe, 'jpg'), dpi=99)
         plt.close()
@@ -97,11 +123,11 @@ def mkdir(output_path: str,
 
     dir_type = ''
     if mode == 'centromeres':
-        dir_type = dir_res + '/cohesins_peaks/'
+        dir_type = dir_res + 'centromeres/'
     if mode == 'cohesins':
-        dir_type = dir_res + '/cohesins_peaks/'
+        dir_type = dir_res + 'cohesins_peaks/'
     if mode == 'telomeres':
-        dir_type = dir_res + '/cohesins_peaks/'
+        dir_type = dir_res + 'telomeres/'
 
     if not os.path.exists(dir_type):
         os.makedirs(dir_type)
