3873c45c02d388c55e1bdb22c73f610164146f07
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2023-01-06 17:39:23 +0100

    deleted

--- scratch/oligos_replacement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-genome="/Users/loqmenanani/OneDrive/ENS/L3_ENS/stage_l3/Projet/oligos_replacement/inputs/S288c_DSB_LY_Capture_original.fa"
-oligos="/Users/loqmenanani/OneDrive/ENS/L3_ENS/stage_l3/Projet/oligos_replacement/inputs/oligo_positions.csv"
-output="/Users/loqmenanani/OneDrive/ENS/L3_ENS/stage_l3/Projet/oligos_replacement/outputs/S288c_DSB_LY_Capture_original_artificial.fa"
-bed="/Users/loqmenanani/OneDrive/ENS/L3_ENS/stage_l3/Projet/oligos_replacement/outputs/chr_artificial_coordinates.bed"
-
-hic_ssdna.oligos_replacement -i $genome -o $output -c $oligos -b $bed -s 150
