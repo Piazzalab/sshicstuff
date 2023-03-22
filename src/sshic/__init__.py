356f6fa95a197ee9724befc346d5f4b041e7366f
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-03-22 15:17:04 +0100

    updated

--- src/sshic/__init__.py
+++ src/sshic/__init__.py
@@ -2,25 +2,24 @@ from .tools import find_nearest
 from .tools import frag2
 from .tools import is_debug
 
-from .filter import run
-from .format import run
+from .filter import main
+from .format import main
 from .statistics import run
 
-from .nucleosomes import plot_size_distribution
 from .nucleosomes import preprocess
-from .nucleosomes import run
+from .nucleosomes import main
 
 from .centromeres import freq_focus_around_centromeres
 from .centromeres import compute_centromere_freq_per_oligo_per_chr
 from .centromeres import compute_average_aggregate
-from .centromeres import run
+from .centromeres import main
 
 from .telomeres import freq_focus_around_telomeres
 from .telomeres import compute_average_aggregate
 from .telomeres import compute_telomere_freq_per_oligo_per_chr
-from .telomeres import run
+from .telomeres import main
 
 from .cohesins import freq_focus_around_cohesin_peaks
 from .cohesins import compute_average_aggregate
-from .cohesins import run
+from .cohesins import main
 
