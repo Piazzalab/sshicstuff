039ad8dbd8d8106c49e1825bc6590d81df342bd5
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2023-03-23 16:26:53 +0100

    WIP : add pondered mutant

--- src/rdna.py
+++ src/rdna.py
@@ -8,18 +8,48 @@ import re
 #   Set as None to avoid SettingWithCopyWarning
 pd.options.mode.chained_assignment = None
 
+
+def main(
+        binned_contacts_path: str,
+        output_dir: str
+):
+
+    samp_id = re.search(r"AD\d+", binned_contacts_path).group()
+    print(samp_id)
+    df_contacts = pd.read_csv(binned_contacts_path, sep='\t')
+    for colname, colfrag in probes_averages.items():
+        df_contacts[colname] = df_contacts[colfrag].mean(axis=1)
+
+    df_rdna_focus = pd.DataFrame()
+    df_rdna_focus['avg_flanking_left_(chr12-440000:450000)'] = \
+        pd.merge(rdna_flanking_left, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
+
+    df_rdna_focus['avg_rdna_region_(chr12-451000:467000)'] = \
+        pd.merge(rdna_regions, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
+
+    df_rdna_focus['avg_flanking_right_(chr12-490000:500000)'] = \
+        pd.merge(rdna_flanking_right, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
+
+    df_rdna_focus.drop(['chr_bins', 'genome_bins'], inplace=True)
+    df_rdna_focus.to_csv(output_dir + samp_id + '_rDNA_regions_frequencies.tsv', sep='\t')
+
+    del df_contacts, df_rdna_focus
+
+
 if __name__ == "__main__":
     data_dir = os.path.dirname(os.getcwd()) + '/data/'
     sshic_pcrdupt_dir = ['sshic/', 'sshic_pcrdupkept/']
+    pondering_mode = ['not_pondered', 'pondered']
 
     outputs_dir = data_dir + 'outputs/'
     inputs_dir = data_dir + 'inputs/'
     binning_dir = outputs_dir + "binned/"
+    pondered_dir = outputs_dir + "pondered/"
     rdna_dir = outputs_dir + "rdna/"
 
     rdna_regions = pd.DataFrame({'chr': ['chr12'] * 17, 'chr_bins': np.arange(451000, 468000, 1000)})
-    rdna_flanking_left = pd.DataFrame({'chr': ['chr12'] * 6, 'chr_bins': np.arange(445000, 451000, 1000)})
-    rdna_flanking_right = pd.DataFrame({'chr': ['chr12'] * 6, 'chr_bins': np.arange(470000, 476000, 1000)})
+    rdna_flanking_left = pd.DataFrame({'chr': ['chr12'] * 11, 'chr_bins': np.arange(440000, 451000, 1000)})
+    rdna_flanking_right = pd.DataFrame({'chr': ['chr12'] * 10, 'chr_bins': np.arange(490000, 500000, 1000)})
 
     probes_averages = {
         'Average_left': ['18535', '18589', '18605', '18611', '18614', '18616'],
@@ -36,29 +66,28 @@ if __name__ == "__main__":
     print("Look at contacts between probes and rDNA regions")
     for sshic_dir in sshic_pcrdupt_dir:
         print(sshic_dir)
-        samples = sorted([f for f in os.listdir(binning_dir+sshic_dir+'1kb/') if 'frequencies.tsv' in f])
-        if not os.path.exists(rdna_dir+sshic_dir):
-            os.makedirs(rdna_dir+sshic_dir)
-        for samp in samples:
-            samp_id = re.search(r"AD\d+", binning_dir+sshic_dir+'1kb/'+samp).group()
-            print(samp_id)
-            df_contacts = pd.read_csv(binning_dir+sshic_dir+'1kb/'+samp, sep='\t')
-            for colname, colfrag in probes_averages.items():
-                df_contacts[colname] = df_contacts[colfrag].mean(axis=1)
+        for ponder in pondering_mode:
+            output_dir = ''
+            samples_dir = ''
+            samples = []
+            if ponder == 'not_pondered':
+                print('not pondered contacts')
+                output_dir = rdna_dir+'not_pondered/'+sshic_dir
+                samples_dir = binning_dir + sshic_dir + '1kb/'
+                samples = sorted([f for f in os.listdir(samples_dir) if 'frequencies' in f])
 
-            df_rdna_focus = pd.DataFrame()
-            df_rdna_focus['avg_flanking_left \n (chr12-445000:450000)'] = \
-                pd.merge(rdna_flanking_left, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
+            elif ponder == 'pondered':
+                print('pondered contacts')
+                output_dir = rdna_dir+'pondered/'+sshic_dir
+                samples_dir = pondered_dir + sshic_dir + '1kb/'
+                samples = sorted([f for f in os.listdir(samples_dir) if 'frequencies' in f])
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-            df_rdna_focus['avg_rdna_region \n (chr12-451000:468000)'] = \
-                pd.merge(rdna_regions, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
-
-            df_rdna_focus['avg_flanking_right \n (chr12-470000:475000)'] = \
-                pd.merge(rdna_flanking_right, df_contacts, on=['chr', 'chr_bins']).mean(axis=0, numeric_only=True)
-
-            df_rdna_focus.drop(['chr_bins', 'genome_bins'], inplace=True)
-
-            df_rdna_focus.to_csv(rdna_dir+sshic_dir+samp_id+'_rDNA_regions_frequencies.tsv', sep='\t')
+            for samp in samples:
+                main(
+                    binned_contacts_path=samples_dir+samp,
+                    output_dir=output_dir
+                )
 
     print('--DONE--')
-
