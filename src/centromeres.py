fix mistake during inter normalisation for aggragated

src/centromeres.py
@@ -69,7 +69,7 @@ def freq_focus_around_centromeres(
             df_contacts.loc[df_contacts['chr'] == probe_chr, f] = np.nan
 
     #   Inter normalization
-    df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
+    df_contacts[fragments] = df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
 
     df_merged = pd.merge(df_contacts, df_centros, on='chr')
     df_merged_cen_areas = df_merged[

src/cohesins.py
@@ -62,7 +62,7 @@ def freq_focus_around_cohesin_peaks(
             df_contacts.loc[df_contacts['chr'] == probe_chr, f] = np.nan
 
     #   Inter normalization
-    df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
+    df_contacts[fragments] = df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
 
     df_merged = pd.merge(df_contacts, df_peaks, on='chr')
     df_merged_cohesins_areas = df_merged[

src/rdna.py
@@ -26,7 +26,7 @@ def main(
         probe_chr = df_probes.loc[df_probes['frag_id'] == int(f), 'chr'].tolist()[0]
         df_contacts.loc[df_contacts['chr'] == probe_chr, f] = np.nan
     #   Inter normalization
-    df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
+    df_contacts[fragments] = df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
 
     for colname, colfrag in probes_averages.items():
         df_contacts[colname] = df_contacts[colfrag].mean(axis=1)

src/telomeres/arm_size.py
@@ -30,7 +30,7 @@ def main(
             df_contacts.loc[df_contacts['chr'] == probe_chr, f] = np.nan
 
     #   Inter normalization
-    df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
+    df_contacts[fragments] = df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
     #   add additional subset of probes where contacts are averaged
     for colname, colfrag in probes_averages.items():
         df_contacts[colname] = df_contacts[colfrag].mean(axis=1)

src/telomeres/telomeres.py
@@ -69,7 +69,7 @@ def freq_focus_around_telomeres(
             df_contacts.loc[df_contacts['chr'] == probe_chr, f] = np.nan
 
     #   Inter normalization
-    df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
+    df_contacts[fragments] = df_contacts[fragments].div(df_contacts[fragments].sum(axis=0))
     df_merged = pd.merge(df_contacts, df_telos, on='chr')
 
     df_merged_telos_areas_part_a = df_merged[