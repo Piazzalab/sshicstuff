579b56e875855213970f713bbc26ab80d1d09668
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-04-05 14:45:34 +0200

    sum fragments : 18535+18589+18605+18611+18614+18666+18694 mean on AD162+AD242+AD296+AD300 center on 80kb around centromeres (both sides)

--- sshic/transcription.py
+++ sshic/transcription.py
@@ -57,39 +57,54 @@ def preprocess(
 def main(
     df_genes: pd.DataFrame,
     df_probes: pd.DataFrame,
+    df_centro: pd.DataFrame,
     fragments_path: str,
     output_dir: str
 ):
     samples_id = re.search(r"AD\d+", fragments_path).group()
     fragments = pd.unique(df_probes['frag_id'].astype(str))
+    fragments_of_interest = ["18535", "18589", "18605", "18611", "18614", "18666", "18694"]
+
     df_contacts = pd.read_csv(fragments_path, sep='\t')
+    df_contacts = df_contacts.loc[:, (df_contacts.columns.isin(fragments_of_interest)) |
+                                     (df_contacts.columns.isin(['chr', 'positions', 'sizes']))]
     df_contacts.insert(2, 'end', df_contacts.positions+df_contacts.sizes)
     df_contacts.rename(columns={'positions': 'start'}, inplace=True)
+    df_contacts["sum"] = df_contacts[fragments_of_interest].sum(axis=1)
+    df_contacts.drop(columns=fragments_of_interest, inplace=True)
 
-    df_merged = df_contacts.merge(df_genes,  on='chr')
-
-    df_merged_filtered = df_merged.loc[
-        (df_merged["start_x"] >= df_merged["start_y"]) &
-        (df_merged["end_x"] <= df_merged["end_y"])
+    df_merged1 = pd.merge(df_contacts, df_centro, on='chr')
+    df_merged_cen = df_merged1[
+        (df_merged1.start > (df_merged1.left_arm_length-80000)) &
+        (df_merged1.start < (df_merged1.left_arm_length+80000))
     ]
 
-    df_merged_filtered_3 = df_merged.loc[
-        (df_merged["start_x"] >= df_merged["start_y"]) &
-        (df_merged["start_x"] <= df_merged["end_y"])
+    df_contacts2 = df_merged_cen.drop(columns=["length", "left_arm_length", "right_arm_length"])
+    df_contacts2.index = range(len(df_contacts2))
+
+    df_merged2 = df_contacts2.merge(df_genes,  on='chr')
+    df_merged_filtered = df_merged2.loc[
+        (df_merged2["start_x"] >= df_merged2["start_y"]) &
+        (df_merged2["end_x"] <= df_merged2["end_y"])
     ]
 
-    df_merged_filtered_5 = df_merged.loc[
-        (df_merged["end_x"] >= df_merged["start_y"]) &
-        (df_merged["end_x"] <= df_merged["end_y"])
+    df_merged_filtered_3 = df_merged2.loc[
+        (df_merged2["start_x"] >= df_merged2["start_y"]) &
+        (df_merged2["start_x"] <= df_merged2["end_y"])
+    ]
+
+    df_merged_filtered_5 = df_merged2.loc[
+        (df_merged2["end_x"] >= df_merged2["start_y"]) &
+        (df_merged2["end_x"] <= df_merged2["end_y"])
     ]
 
     df_filtered = pd.concat((df_merged_filtered, df_merged_filtered_3, df_merged_filtered_5))
     df_grouped = \
         df_filtered.groupby(by=['chr', 'start_y', 'end_y'], as_index=False).mean(numeric_only=True)
 
-    df_grouped.drop(columns=['start_x', 'end_x', 'sizes', 'strand', 'length', 'rna_per_bp'], inplace=True)
+    df_grouped.drop(columns=['start_x', 'end_x', 'sizes', ], inplace=True)
     df_grouped.rename(columns={'start_y': 'start', 'end_y': 'end'}, inplace=True)
-    df_grouped_named = df_genes.merge(df_grouped, on=['chr', 'start', 'end'])
+    df_grouped_named = df_genes.merge(df_grouped, on=['chr', 'start', 'end', 'strand', 'length', 'rna_per_bp'])
     df_grouped_named.drop(columns=["Systemati_name", "strand"], inplace=True)
     df_grouped_named.sort_values(by='rna_per_bp', inplace=True)
     df_grouped_named.index = range(len(df_grouped_named))
@@ -102,6 +117,29 @@ def main(
     df_bottom_10.to_csv(output_dir+samples_id+'_bottom_10_percent.tsv', sep='\t')
     df_top_10.to_csv(output_dir+samples_id+'_top_10_percent.tsv', sep='\t')
 
+    return df_grouped_named, df_bottom_10, df_top_10
+
+
+def merge(
+        wt_res: dict,
+        output_dir: str
+):
+    df_merged = pd.DataFrame()
+    df_merged_b10 = pd.DataFrame()
+    df_merged_t10 = pd.DataFrame()
+    for wt, tpl in wt_res.items():
+        df_merged = pd.concat((df_merged, tpl[0]))
+        df_merged_b10 = pd.concat((df_merged_b10, tpl[1]))
+        df_merged_t10 = pd.concat((df_merged_t10, tpl[2]))
+
+    df_merged = df_merged.groupby(by=["chr", "start", "end"], as_index=False).mean(numeric_only=True)
+    df_merged_b10 = df_merged_b10.groupby(by=["chr", "start", "end"], as_index=False).mean(numeric_only=True)
+    df_merged_t10 = df_merged_t10.groupby(by=["chr", "start", "end"], as_index=False).mean(numeric_only=True)
+
+    df_merged.to_csv(output_dir+"Average_"+'-'.join(list(wt_res.keys()))+'_all_genes.tsv', sep='\t')
+    df_merged_b10.to_csv(output_dir+"Average_"+'-'.join(list(wt_res.keys()))+'_bottom_10_percent.tsv', sep='\t')
+    df_merged_t10.to_csv(output_dir+"Average_"+'-'.join(list(wt_res.keys()))+'_top_10_percent.tsv', sep='\t')
+
 
 if __name__ == "__main__":
 
@@ -112,11 +150,13 @@ if __name__ == "__main__":
     binning_dir = outputs_dir + "binned/"
     transcript_regions_bed_path = inputs_dir + "GSM2790503_BAR5_AM1105_tension_WT.bed"
     genes_with_score_path = inputs_dir + "genes_list_with_coverage_per_bp.tsv"
+    centromeres_positions = inputs_dir + "S288c_chr_centro_coordinates.tsv"
     transcripts_dir = outputs_dir + "/transcripts/"
     sshic_pcrdupt_dir = ['sshic/', 'sshic_pcrdupkept/']
 
     df_genes_scored = pd.read_csv(genes_with_score_path, sep='\t', index_col=0)
     df_probes2frag = pd.read_csv(probes_and_fragments, sep='\t', index_col=0)
+    df_centro_pos = pd.read_csv(centromeres_positions, sep='\t', index_col=None)
 
     for sshic_dir in sshic_pcrdupt_dir:
         print(sshic_dir)
@@ -125,12 +165,23 @@ if __name__ == "__main__":
 
         if not os.path.exists(transcripts_dir+sshic_dir):
             os.makedirs(transcripts_dir+sshic_dir)
+
+        wt_df = {}
         for samp in samples:
-            main(
-                df_genes=df_genes_scored,
-                df_probes=df_probes2frag,
-                fragments_path=not_binned_dir+samp,
-                output_dir=transcripts_dir+sshic_dir
-            )
-    pass
+            samp_id = re.search(r"AD\d+", samp).group()
+            if samp_id in ["AD162", "AD242", "AD296", "AD300"]:
+                print(samp_id)
+                df, df_b10, df_t10 = main(
+                    df_genes=df_genes_scored,
+                    df_probes=df_probes2frag,
+                    df_centro=df_centro_pos,
+                    fragments_path=not_binned_dir+samp,
+                    output_dir=transcripts_dir+sshic_dir
+                )
+                wt_df[samp_id] = (df, df_b10, df_t10)
+
+        merge(wt_res=wt_df, output_dir=transcripts_dir+sshic_dir)
+
+
+
 
