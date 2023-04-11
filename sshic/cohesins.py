5351c3bda344d2279b71e945f79424bb8cda20dd
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-04-11 16:54:03 +0200

    WIP : cohesins peaks intervals

--- sshic/cohesins.py
+++ sshic/cohesins.py
@@ -4,6 +4,8 @@ import pandas as pd
 import os
 import re
 
+import universal.utils as tools
+
 #   Set as None to avoid SettingWithCopyWarning
 pd.options.mode.chained_assignment = None
 
@@ -15,51 +17,25 @@ def main(
         fragments_path: str,
         output_dir: str
 ):
-
     samples_id = re.search(r"AD\d+", fragments_path).group()
     fragments = pd.unique(df_probes['frag_id'].astype(str))
     fragments_of_interest = ["18535", "18589", "18605", "18611", "18614", "18666", "18694"]
 
     df_contacts = pd.read_csv(fragments_path, sep='\t')
-    bin_size = int(df_contacts.chr_bins[1]-df_contacts.chr_bins[0])
+    bin_size = int(df_contacts.chr_bins[1] - df_contacts.chr_bins[0])
     df_contacts = df_contacts.loc[:, (df_contacts.columns.isin(fragments_of_interest)) |
                                      (df_contacts.columns.isin(['chr', 'chr_bins', 'sizes']))]
-    df_contacts.insert(2, 'end', df_contacts.chr_bins+bin_size)
+    df_contacts.insert(2, 'chr_bins_end', df_contacts.chr_bins + bin_size)
     df_contacts["sum"] = df_contacts[fragments_of_interest].sum(axis=1)
     df_contacts.drop(columns=fragments_of_interest, inplace=True)
 
-    df_merged1 = df_contacts.merge(df_centro, on='chr')
-    df_merged_cen = df_merged1[
-        (df_merged1.chr_bins > (df_merged1.left_arm_length-80000-bin_size)) &
-        (df_merged1.chr_bins < (df_merged1.left_arm_length+80000))
+    df_merged = pd.merge(df_peaks, df_contacts, on='chr')
+    df_filtered = df_merged.loc[
+        (df_merged['inter'] >= df_merged['chr_bins']) &
+        (df_merged['inter'] < df_merged['chr_bins_end'])
     ]
-
-    df_contacts2 = df_merged_cen.drop(columns=["length", "left_arm_length", "right_arm_length"])
-    df_contacts2.index = range(len(df_contacts2))
-
-    df_merged2 = df_contacts2.merge(df_peaks, on='chr')
-    df_merged_cohesins_areas = df_merged2[
-        (df_merged2.start >= df_merged2.chr_bins) &
-        (df_merged2.start + 1 <= df_merged2.end)
-    ]
-    df_merged_cohesins_areas.drop(columns=['end', 'uid'], axis=1, inplace=True)
-    df_merged_cohesins_areas.rename(columns={'start': 'cohesin'}, inplace=True)
-
-    df_enrichment_cohesins = df_merged_cohesins_areas[['chr', 'chr_bins', 'cohesin', 'score']]
-    df_enrichment_cohesins['enrichment'] = np.nan
-    for index, row in df_merged_cohesins_areas.iterrows():
-        cohesins_bin_contact_per_bp = row['sum'] / 1000
-        df_extended_region = df_contacts2.loc[
-            (df_contacts2['chr_bins'] >= row['chr_bins'] - 3000) &
-            (df_contacts2['end'] <= row['chr_bins'] + 4000) &
-            (df_contacts2['chr'] == row['chr'])
-        ]
-
-        extended_region_contacts_per_bp = df_extended_region['sum'].sum() / 7000
-        df_enrichment_cohesins.loc[index, 'enrichment'] = cohesins_bin_contact_per_bp / extended_region_contacts_per_bp
 
     pass
-
 
 
 if __name__ == "__main__":
@@ -78,6 +54,45 @@ if __name__ == "__main__":
     df_cohesins_peaks = pd.read_csv(cohesins_peaks_path, sep='\t', index_col=None,
                                     names=['chr', 'start', 'end', 'uid', 'score'])
     df_cohesins_peaks.drop(columns='end', inplace=True)
+    df_cohesins_peaks = tools.sort_by_chr(df_cohesins_peaks, col1='chr', col2='start')
+
+    df_cohesins_peaks.drop(['uid', 'score'], axis=1, inplace=True)
+    df_inter_peaks_10kb = pd.DataFrame()
+    for chrom in pd.unique(df_cohesins_peaks['chr']):
+        df = df_cohesins_peaks.loc[df_cohesins_peaks['chr'] == chrom]
+        while True:
+            if not any(df['start'].diff() < 10000):
+                break
+            df['end'] = df['start'].shift(-1) - 1
+            df = df.iloc[:-1, :]
+            df['size'] = df['end'] - df['start'] + 1
+            df_filtered = df.loc[df['size'] >= 10000]
+            df = df_filtered[['chr', 'start', 'end', 'size']]
+            df = pd.concat(
+                (df, pd.DataFrame({'chr': [chrom], 'start': [df_filtered.iloc[-1, 2]+1]})), axis=0, ignore_index=True)
+        df_inter_peaks_10kb = pd.concat((df_inter_peaks_10kb, df))
+    del df_filtered, df
+
+    df_inter_peaks_10kb.dropna(axis=0, inplace=True)
+    df_inter_peaks_10kb.index = range(len(df_inter_peaks_10kb))
+    df_inter_peaks_10kb[['start', 'end', 'size']] = df_inter_peaks_10kb[['start', 'end', 'size']].astype('int64')
+    df_inter_peaks_10kb['bin_start'] = (df_inter_peaks_10kb['start'] // 1000) * 1000
+    df_inter_peaks_10kb['bin_end'] = (df_inter_peaks_10kb['end'] // 1000) * 1000
+    df_inter_peaks_10kb['bin_diff'] = df_inter_peaks_10kb['bin_end'] - df_inter_peaks_10kb['bin_start']
+
+    rebin = df_inter_peaks_10kb['bin_diff'].min() // 1000 + 1
+    new_df = pd.DataFrame()
+    for index, row in df_inter_peaks_10kb.iterrows():
+        interval_linspace = np.linspace(row['bin_start'], row['bin_end'], rebin).astype('int64')
+        interval_size = int(np.diff(interval_linspace).mean())
+        duplicated_rows = pd.concat([row] * len(interval_linspace), axis=1).T.reset_index(drop=True)
+        duplicated_rows['inter'] = interval_linspace
+        duplicated_rows['inter_size'] = interval_size
+        duplicated_rows['inter_shifted'] = (duplicated_rows['inter'] - row['bin_start']) // interval_size
+
+        new_df = pd.concat([new_df, duplicated_rows], ignore_index=False)
+
+    new_df.drop(columns=['bin_start', 'bin_end', 'bin_diff', 'size'], inplace=True)
     df_probes2frag = pd.read_csv(probes_and_fragments, sep='\t', index_col=0)
     df_centro_pos = pd.read_csv(centromeres_positions, sep='\t', index_col=None)
 
@@ -95,7 +110,7 @@ if __name__ == "__main__":
             if samp_id in ["AD162", "AD242", "AD296", "AD300"]:
                 print(samp_id)
                 main(
-                    df_peaks=df_cohesins_peaks,
+                    df_peaks=new_df,
                     df_probes=df_probes2frag,
                     df_centro=df_centro_pos,
                     fragments_path=not_binned_dir+samp,
