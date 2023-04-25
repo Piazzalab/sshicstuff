770d218f10c810fc07e415435336660b85f648e7
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-04-25 14:44:17 +0200

    WIP

--- sshic/gui/app.py
+++ sshic/gui/app.py
@@ -7,8 +7,6 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
-# Create a Dash application instance:
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Load and set the data folders and files
 data_dir = "../../test_data/sshic/AD162/"
@@ -21,96 +19,44 @@ binned_dfs = {file: pd.read_csv(os.path.join(data_dir, file), sep='\t') for file
 not_a_probe_columns = ["chr", "sizes", "start", "chr_bins", "genome_bins"]
 probes = [col for col in binned_dfs[binned_files[0]].columns.values if col not in not_a_probe_columns]
 
-# Define the layout of the application:
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            html.Label("Select probes:"),
-            dcc.Dropdown(
-                id='probe-selector',
-                options=[{'label': probe, 'value': probe} for probe in probes],
-                value=[probes[0], probes[1]],
-                multi=True,
-            ),
-            html.Label("Select binning:"),
-            dcc.Dropdown(
-                id='binning-selector',
-                options=[{'label': file, 'value': file} for file in binned_files],
-                value=binned_files[0]
-            ),
-            html.Br(),
-        ], width=3),
-    ], style={'margin-top': '10px'}),
-
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='graph1', config={'displayModeBar': True, 'scrollZoom': True}),
-        ], width=12),
-    ], style={'margin-top': '20px'}),
-
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='graph2', config={'displayModeBar': True, 'scrollZoom': True}),
-        ], width=12),
-    ], style={'margin-top': '20px'})
-])
+# Create a Dash application instance:
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
-@app.callback(
-    Output('probe-selector', 'value'),
-    [Input('probe-selector', 'value')]
-)
-def limit_probe_selection(selected_probes):
-    if len(selected_probes) > 2:
-        return selected_probes[:2]
-    return selected_probes
-
-
-@app.callback(
-    Output('graph1', 'figure'),
-    [Input('probe-selector', 'value'),
-     Input('binning-selector', 'value')]
-)
-def update_figure_1(selected_probes, selected_binning):
-    if len(selected_probes) != 2:
+def update_figure(selected_probe, selected_binning, fig):
+    if not selected_probe:
         return go.Figure()
+
     df = binned_dfs[selected_binning]
-    return create_figure(df, selected_probes[0])
-
-
-@app.callback(
-    Output('graph2', 'figure'),
-    [Input('probe-selector', 'value'),
-     Input('binning-selector', 'value')]
-)
-def update_figure_2(selected_probes, selected_binning):
-    if len(selected_probes) != 2:
-        return go.Figure()
-    df = binned_dfs[selected_binning]
-    return create_figure(df, selected_probes[1])
-
-
-def create_figure(df, probe):
     x_col = "genome_bins"
-    fig = go.Figure()
+    fig = go.Figure(fig)  # Create a copy of the input figure
+    fig.update_traces(visible=False)  # Hide existing traces
 
-    fig.add_trace(
-        go.Scattergl(
-            x=df[x_col],
-            y=df[probe],
-            name=probe,
-            mode='lines+markers',
-            line=dict(width=1),
-            marker=dict(size=4)
+    probe_traces = [trace for trace in fig.data if trace['name'] == selected_probe]
+    if len(probe_traces) == 0:
+        # Add trace if it does not already exist
+        fig.add_trace(
+            go.Scattergl(
+                x=df[x_col],
+                y=df[selected_probe],
+                name=selected_probe,
+                mode='lines+markers',
+                line=dict(width=1),
+                marker=dict(size=4)
+            )
         )
-    )
+
+    # Show only the trace for the selected probe
+    for trace in fig.data:
+        if trace['name'] == selected_probe:
+            trace.visible = True
 
     fig.update_layout(
         width=4000,
-        height=400,
+        height=600,
         xaxis=dict(title="Genome bins"),
         yaxis=dict(title="Contact frequency"),
-        hovermode='closest',
+        hovermode='closest'
     )
 
     # Adding a vertical line to show chromosome
@@ -136,27 +82,71 @@ def create_figure(df, probe):
             ),
             xref="x"
         )
+
     return fig
 
 
-@app.callback(
-    Output('graph1', 'relayoutData'),
-    [Input('graph2', 'relayoutData')]
-)
-def update_graph1_from_graph2(relayout_data):
-    if relayout_data is None:
-        raise dash.exceptions.PreventUpdate
-    return relayout_data
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.Label("Select probe:"),
+            dcc.Dropdown(
+                id='probe-selector',
+                options=[{'label': probe, 'value': probe} for probe in probes],
+                value=probes[0]
+            ),
+            html.Label("Select binning:"),
+            dcc.Dropdown(
+                id='binning-selector',
+                options=[{'label': file, 'value': file} for file in binned_files],
+                value=binned_files[0]
+            ),
+            html.Br(),
+        ], width=3, style={'position': 'absolute', 'top': '10px', 'left': '10px'}),
+    ], style={'margin-top': '10px'}),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='graph1', config={'displayModeBar': True, 'scrollZoom': True}),
+        ], width=12, align='left'),
+    ], style={'margin-top': '200px', 'margin-left': '0px'}),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='graph2', config={'displayModeBar': True, 'scrollZoom': True}),
+        ], width=12, align='left'),
+    ], style={'margin-top': '200px', 'margin-left': '0px'})
+])
 
 
 @app.callback(
-    Output('graph2', 'relayoutData'),
-    [Input('graph1', 'relayoutData')]
+    Output('probe-selector', 'value'),
+    [Input('probe-selector', 'value')]
 )
-def update_graph2_from_graph1(relayout_data):
-    if relayout_data is None:
-        raise dash.exceptions.PreventUpdate
-    return relayout_data
+def limit_probe_selection(selected_probes):
+    if len(selected_probes) > 2:
+        return selected_probes[:2]
+    return selected_probes
+
+
+@app.callback(
+    Output('graph1', 'figure'),
+    [Input('probe-selector', 'value'),
+     Input('binning-selector', 'value'),
+     Input('graph2', 'figure')]
+)
+def update_graph1(selected_probe, selected_binning, graph2_figure):
+    return update_figure(selected_probe, selected_binning, graph2_figure)
+
+
+@app.callback(
+    Output('graph2', 'figure'),
+    [Input('probe-selector', 'value'),
+     Input('binning-selector', 'value'),
+     Input('graph1', 'figure')]
+)
+def update_graph2(selected_probe, selected_binning, graph1_figure):
+    return update_figure(selected_probe, selected_binning, graph1_figure)
 
 
 if __name__ == '__main__':
