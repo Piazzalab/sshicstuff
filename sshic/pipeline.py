74d2a49996460c9df35cb27c46f591eea1b9902d
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-05-16 16:22:22 +0200

    remade a pipeline but that takes only one sample at a time

--- sshic/pipeline.py
+++ sshic/pipeline.py
@@ -1,8 +1,6 @@
 import re
 import os
 import argparse
-
-import pandas as pd
 from typing import List
 
 from filter import filter_contacts
@@ -13,30 +11,28 @@ from statistics import get_stats, compare_to_wt
 from binning import rebin_contacts
 from ponder import ponder_mutant
 from aggregated import aggregate
-from utils import is_debug
 
 
 class PathBundle:
-    def __init__(self, sample_sparse_file_path, samples_wt_table_path: str):
+    def __init__(self, sample_sparse_file_path: str, reference_path):
+
         self.sample_sparse_file_path = sample_sparse_file_path
-        self.sample_input_dir = os.path.dirname(sample_sparse_file_path)
         self.samp_id = re.match(r'^AD\d+', sample_sparse_file_path.split("/")[-1]).group()
-        self.sample_output_dir = os.path.join(self.sample_input_dir, self.samp_id)
-        os.makedirs(self.sample_output_dir, exist_ok=True)
+        parent_dir = os.path.dirname(sample_sparse_file_path)
+        self.sample_dir = os.path.join(parent_dir, self.samp_id)
+        os.makedirs(self.sample_dir, exist_ok=True)
 
-        self.filtered_contacts_input = os.path.join(self.sample_output_dir, self.samp_id + "_filtered.tsv")
-        self.cover = os.path.join(self.sample_output_dir, self.samp_id + "_coverage_per_fragment.bedgraph")
-        self.unbinned_contacts_input = os.path.join(self.sample_output_dir, self.samp_id+"_unbinned_contacts.tsv")
-        self.unbinned_frequencies_input = os.path.join(self.sample_output_dir, self.samp_id+"_unbinned_frequencies.tsv")
-        self.global_statistics_input = os.path.join(self.sample_output_dir, self.samp_id+"_global_statistics.tsv")
+        self.filtered_contacts_input = os.path.join(self.sample_dir, self.samp_id + "_filtered.tsv")
+        self.cover = os.path.join(self.sample_dir, self.samp_id + "_coverage_per_fragment.bedgraph")
+        self.unbinned_contacts_input = os.path.join(self.sample_dir, self.samp_id+"_unbinned_contacts.tsv")
+        self.unbinned_frequencies_input = os.path.join(self.sample_dir, self.samp_id+"_unbinned_frequencies.tsv")
+        self.global_statistics_input = os.path.join(self.sample_dir, self.samp_id+"_global_statistics.tsv")
 
-        mutants_vs_wt: pd.DataFrame = pd.read_csv(samples_wt_table_path, sep="\t")
-        ref = mutants_vs_wt[mutants_vs_wt["sample"] == self.samp_id]["ref"].tolist()[0]
-        if re.match(r'^AD\d+', ref):
-            self.wt_to_compare_path = \
-                os.path.join(self.sample_input_dir, os.path.join(ref, os.path.join(f"{ref}_global_statistics.tsv")))
+        if not os.path.exists(reference_path):
+            raise ValueError(f"file {reference_path} doesnt exist. "
+                             f"Please be sure to first run the pipeline on the reference sample before")
         else:
-            self.wt_to_compare_path = os.path.join(self.sample_input_dir, f"wt/{ref}.tsv")
+            self.wt_to_compare_path = reference_path
 
 
 class AggregateParams:
@@ -52,102 +48,91 @@ def check_and_run(output_path, func, *args):
         func(*args)
 
 
-def step_one(
-        path_bundle: PathBundle,
-        oligos_path: str,
-        fragments_list_path: str
+def do_it(
+    path_bundle: PathBundle,
+    oligos_path: str,
+    fragments_list_path: str,
+    centromeres_coordinates_path: str,
+    binning_size_list: List[int],
+    aggregate_params_centros: AggregateParams,
+    aggregate_params_telos: AggregateParams,
 ):
+    print(f" -- Sample {path_bundle.samp_id} -- \n")
+    print(f"Filter contacts \n")
+    check_and_run(
+        path_bundle.filtered_contacts_input, filter_contacts, oligos_path,
+        fragments_list_path, path_bundle.sample_sparse_file_path, path_bundle.sample_dir)
 
-    check_and_run(path_bundle.filtered_contacts_input,
-                  filter_contacts,
-                  oligos_path,
-                  fragments_list_path,
-                  path_bundle.sample_sparse_file_path,
-                  path_bundle.sample_output_dir)
-
+    print(f"Associate the fragment name to probe where it is located \n")
     associate_probes_to_fragments(fragments_list_path, oligos_path)
 
-    check_and_run(path_bundle.cover, coverage,
-                  path_bundle.sample_sparse_file_path,
-                  fragments_list_path, path_bundle.sample_output_dir)
+    print(f"Make the coverage \n")
+    check_and_run(
+        path_bundle.cover, coverage, path_bundle.sample_sparse_file_path, fragments_list_path, path_bundle.sample_dir)
 
+    print(f"Organize the contacts between probe fragments and the rest of the genome 'unbinned tables' \n")
     organize_contacts(filtered_contacts_path=path_bundle.filtered_contacts_input, oligos_path=oligos_path)
 
-    get_stats(contacts_unbinned_path=path_bundle.unbinned_contacts_input,
-              sparse_contacts_path=path_bundle.sample_sparse_file_path,
-              oligos_path=oligos_path)
+    print(f"Make basic statistics on the contacts (inter/intra chr, cis/trans, ssdna/dsdna etc ...) \n")
+    get_stats(
+        contacts_unbinned_path=path_bundle.unbinned_contacts_input,
+        sparse_contacts_path=path_bundle.sample_sparse_file_path,
+        oligos_path=oligos_path)
 
+    print(f"Compare the capture efficiency with that of a wild type (may be another sample) \n")
+    compare_to_wt(statistics_path=path_bundle.global_statistics_input, reference_path=path_bundle.wt_to_compare_path)
 
-def step_two(
-        path_bundle: PathBundle,
-        oligos_path: str,
-        centromeres_coordinates_path: str,
-        binning_size_list: List[int],
-        aggregate_params_centros: AggregateParams,
-        aggregate_params_telos: AggregateParams,
-):
+    print(f"Ponder the unbinned contacts and frequencies tables by the efficiency score got on step ahead \n")
+    ponder_mutant(
+        statistics_path=path_bundle.global_statistics_input, contacts_path=path_bundle.unbinned_contacts_input,
+        frequencies_path=path_bundle.unbinned_frequencies_input, binned_type="unbinned")
 
-    compare_to_wt(path_bundle.global_statistics_input, path_bundle.wt_to_compare_path)
-
-    ponder_mutant(statistics_path=path_bundle.global_statistics_input,
-                  contacts_path=path_bundle.unbinned_contacts_input,
-                  frequencies_path=path_bundle.unbinned_frequencies_input,
-                  binned_type="unbinned")
-
+    print(f"Rebin and ponder the unbinned tables (contacts and frequencies) at : \n")
     for bn in binning_size_list:
-        rebin_contacts(contacts_unbinned_path=path_bundle.unbinned_contacts_input,
-                       chromosomes_coord_path=centromeres_coordinates_path,
-                       bin_size=bn)
         bin_suffix = str(bn // 1000) + "kb"
+        print(bin_suffix)
+        rebin_contacts(contacts_unbinned_path=path_bundle.unbinned_contacts_input,
+                       chromosomes_coord_path=centromeres_coordinates_path, bin_size=bn)
         binned_contacts_input = \
-            os.path.join(path_bundle.sample_output_dir, path_bundle.samp_id + f"_{bin_suffix}_binned_contacts.tsv")
+            os.path.join(path_bundle.sample_dir, path_bundle.samp_id + f"_{bin_suffix}_binned_contacts.tsv")
         binned_frequencies_input = \
-            os.path.join(path_bundle.sample_output_dir, path_bundle.samp_id + f"_{bin_suffix}_binned_frequencies.tsv")
+            os.path.join(path_bundle.sample_dir, path_bundle.samp_id + f"_{bin_suffix}_binned_frequencies.tsv")
 
         ponder_mutant(
-            statistics_path=path_bundle.global_statistics_input,
-            contacts_path=binned_contacts_input,
-            frequencies_path=binned_frequencies_input,
-            binned_type=f"{bin_suffix}_binned"
-        )
+            statistics_path=path_bundle.global_statistics_input, contacts_path=binned_contacts_input,
+            frequencies_path=binned_frequencies_input, binned_type=f"{bin_suffix}_binned")
+    print("\n")
 
+    print("Make an aggregated of contacts around centromeres \n")
     aggregate(
-        binned_contacts_path=os.path.join(
-            path_bundle.sample_output_dir,
-            path_bundle.samp_id+"_10kb_binned_frequencies.tsv"),
-        centros_coord_path=centromeres_coordinates_path,
-        oligos_path=oligos_path,
-        window_size=aggregate_params_centros.window_size,
-        on="centromeres",
+        binned_contacts_path=os.path.join(path_bundle.sample_dir, path_bundle.samp_id+"_10kb_binned_frequencies.tsv"),
+        centros_coord_path=centromeres_coordinates_path, oligos_path=oligos_path,
+        window_size=aggregate_params_centros.window_size, on="centromeres",
         exclude_probe_chr=aggregate_params_centros.excluded_probe_chr,
         excluded_chr_list=aggregate_params_centros.excluded_chr_list,
-        inter_normalization=aggregate_params_centros.inter_normalization,
-        plot=True)
+        inter_normalization=aggregate_params_centros.inter_normalization, plot=True)
 
+    print("Make an aggregated of contacts around telomeres \n")
     aggregate(
-        binned_contacts_path=os.path.join(path_bundle.sample_output_dir,
-                                          path_bundle.samp_id+"_10kb_binned_frequencies.tsv"),
-        centros_coord_path=centromeres_coordinates_path,
-        oligos_path=oligos_path,
-        window_size=aggregate_params_telos.window_size,
-        on="telomeres",
+        binned_contacts_path=os.path.join(path_bundle.sample_dir, path_bundle.samp_id+"_10kb_binned_frequencies.tsv"),
+        centros_coord_path=centromeres_coordinates_path, oligos_path=oligos_path,
+        window_size=aggregate_params_telos.window_size, on="telomeres",
         exclude_probe_chr=aggregate_params_telos.excluded_probe_chr,
         excluded_chr_list=aggregate_params_telos.excluded_chr_list,
-        inter_normalization=aggregate_params_telos.inter_normalization,
-        plot=True)
+        inter_normalization=aggregate_params_telos.inter_normalization, plot=True)
+
+    print(f"--- {path_bundle.samp_id} DONE --- \n\n")
 
 
 if __name__ == "__main__":
 
-    #   Example :
-
+    #   Command to enter for parameters (parse)
     """
-    -s ../data/AD401_AD407_classic
-    -f ../data/AD401_AD407_classic/fragments_list_S288c_DSB_LY_Capture_artificial_DpnIIHinfI.txt
-    -c ../data/AD401_AD407_classic/S288c_chr_centro_coordinates.tsv 
-    -o ../data/AD401_AD407_classic/capture_oligo_positions.csv
-    --samples-vs-wt ../data/AD401_AD407_classic/mutants_vs_ref.csv
-    -t 15
+    -s ../data/samples/pcrfree/AD404_S288c_DSB_LY_Capture_artificial_cutsite_PCRfree_q20.txt
+    -f ../data/samples/inputs/fragments_list_S288c_DSB_LY_Capture_artificial_DpnIIHinfI.txt
+    -c ../data/samples/inputs/S288c_chr_centro_coordinates.tsv 
+    -o ../data/samples/inputs/capture_oligo_positions.csv
+    -r ../data/samples/inputs/wt4h_pcrfree.tsv
     -b 1000 2000 3000 5000 10000 20000 40000 50000 80000 10000
     --window-size-centros 150000  
     --window-size-telos 150000 
@@ -159,13 +144,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Script that processes sshic samples data.")
 
-    parser.add_argument('-s', '--samples-dir', type=str, required=True,
-                        help='Path to inputs directory that contains samples files')
+    parser.add_argument('-s', '--sparse', type=str, required=True,
+                        help='Path hicstuff sparse matrix output ')
 
     parser.add_argument('-o', '--oligos-input', type=str, required=True,
                         help='Path to the file that contains positions of oligos')
 
-    parser.add_argument('-f', '--fragments-list-input', type=str, required=True,
+    parser.add_argument('-f', '--fragments-list', type=str, required=True,
                         help='Path to the file fragments_list (hic_stuff output)')
 
     parser.add_argument('-c', '--centromeres-coordinates-input', type=str, required=True,
@@ -174,11 +159,8 @@ if __name__ == "__main__":
     parser.add_argument('-b', '--binning-sizes-list', nargs='+', type=int, required=True,
                         help='desired bin size for the rebin step')
 
-    parser.add_argument('-t', '--threads', type=int, required=True,
-                        help='desired number of thread to parallelize')
-
-    parser.add_argument('--samples-vs-wt', type=str, required=True,
-                        help="Path to the table that associates for each sample a WT dir.")
+    parser.add_argument('-r', '--reference', type=str, required=False,
+                        help="Path to the reference WT to ponder the sample.")
 
     parser.add_argument('--window-size-centros', type=int, required=True,
                         help="window (in bp) that defines a focus region to aggregated centromeres")
@@ -198,33 +180,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if is_debug():
-        args.threads = 1
+    sample_path_bundle = PathBundle(args.sparse, args.reference)
+    sample_aggregate_params_centros = AggregateParams(
+        args.window_size_centros, args.exclude_probe_chr, args.excluded_chr, args.inter_norm)
+    sample_aggregate_params_telos = AggregateParams(
+        args.window_size_telos, args.exclude_probe_chr, args.excluded_chr, args.inter_norm)
 
-    list_files = [f for f in os.listdir(args.samples_dir) if os.path.isfile(os.path.join(args.samples_dir, f))]
-    my_samples = [os.path.join(args.samples_dir, s) for s in list_files if re.match(r'^AD\d+', s)]
-
-    step_one_list = []
-    step_two_list = []
-    for sample_file in my_samples:
-        sample_path_bundle = PathBundle(sample_file, args.samples_vs_wt)
-
-        sample_aggregate_params_centros = AggregateParams(args.window_size_centros, args.exclude_probe_chr,
-                                                          args.excluded_chr, args.inter_norm)
-        sample_aggregate_params_telos = AggregateParams(args.window_size_telos, args.exclude_probe_chr,
-                                                        args.excluded_chr, args.inter_norm)
-
-        step_one_list.append((sample_path_bundle, args.oligos_input, args.fragments_list_input))
-
-        step_two_list.append((sample_path_bundle, args.oligos_input, args.centromeres_coordinates_input,
-                              args.binning_sizes_list, sample_aggregate_params_centros, sample_aggregate_params_telos))
-
-    print("step 1 : ")
-    for single_sample_data in step_one_list:
-        print(single_sample_data[0].samp_id)
-        step_one(*single_sample_data)
-
-    print("step 2 : ")
-    for single_sample_data in step_two_list:
-        print(single_sample_data[0].samp_id)
-        step_two(*single_sample_data)
+    sample_data = [sample_path_bundle, args.oligos_input, args.fragments_list, args.centromeres_coordinates_input,
+                   args.binning_sizes_list, sample_aggregate_params_centros, sample_aggregate_params_telos]
+    do_it(*sample_data)
