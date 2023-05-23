74bb3291406df22ef420989c8648db012cbf3c31
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-05-23 16:39:14 +0200

    minor refacto

--- sshic/coverage.py
+++ sshic/coverage.py
@@ -35,20 +35,23 @@ def coverage(
     df_fragments: pd.DataFrame = pd.read_csv(fragments_path, sep='\t')
     df_fragments.rename(columns={'chrom': 'chr', 'start_pos': 'start', 'end_pos': 'end'}, inplace=True)
     df_fragments['id'] = df_fragments.index.values
-    df_hic_contacts: pd.DataFrame = pd.read_csv(hic_contacts_path, header=0, sep="\t", names=['frag_a', 'frag_b', 'contacts'])
+    df_hic_contacts: pd.DataFrame = pd.read_csv(
+        hic_contacts_path, header=0, sep="\t", names=['frag_a', 'frag_b', 'contacts'])
 
     df_coverage: pd.DataFrame = df_fragments[['chr', 'start', 'end']]
     df_coverage['contacts'] = np.nan
 
-    df_merged_a: pd.DataFrame = df_hic_contacts.merge(df_fragments[['id', 'chr', 'start', 'end']],
-                                        left_on='frag_a',
-                                        right_on='id',
-                                        suffixes=('', '_a')).drop(columns=['frag_a', 'frag_b'])
+    df_merged_a: pd.DataFrame = df_hic_contacts.merge(
+        df_fragments[['id', 'chr', 'start', 'end']],
+        left_on='frag_a',
+        right_on='id',
+        suffixes=('', '_a')).drop(columns=['frag_a', 'frag_b'])
 
-    df_merged_b: pd.DataFrame = df_hic_contacts.merge(df_fragments[['id', 'chr', 'start', 'end']],
-                                        left_on='frag_b',
-                                        right_on='id',
-                                        suffixes=('', '_b')).drop(columns=['frag_a', 'frag_b'])
+    df_merged_b: pd.DataFrame = df_hic_contacts.merge(
+        df_fragments[['id', 'chr', 'start', 'end']],
+        left_on='frag_b',
+        right_on='id',
+        suffixes=('', '_b')).drop(columns=['frag_a', 'frag_b'])
 
     df_grouped_a: pd.DataFrame = df_merged_a.groupby(by=['id', 'chr', 'start', 'end'], as_index=False).sum()
     df_grouped_b: pd.DataFrame = df_merged_b.groupby(by=['id', 'chr', 'start', 'end'], as_index=False).sum()
