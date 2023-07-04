fixed a no coherent average (average left/ right etc ...) for probes if the probes underwent a normalization chr_exclusion .

sshic/aggregated.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 import os
 from os.path import join 
 from typing import List, Optional
-from utils import sort_by_chr
+from utils import sort_by_chr, make_groups_of_probes
 
 #   Set as None to avoid SettingWithCopyWarning
 pd.options.mode.chained_assignment = None
@@ -36,6 +36,7 @@ def aggregate(
         output_dir: str,
         excluded_chr_list: Optional[List[str]] = None,
         exclude_probe_chr: bool = True,
+        additional_path: Optional[str] = None,
         inter_normalization: bool = True,
         plot: bool = True
 ):
@@ -62,6 +63,8 @@ def aggregate(
         List of chromosomes to exclude to prevent bias of contacts.
     exclude_probe_chr : bool, optional, default=True
         Exclude the chromosome where the probe comes from (oligo's chromosome).
+    additional_path: str
+        Path to a csv file that contains groups of probes to sum, average etc ...
     inter_normalization : bool, optional, default=True
         Normalize the contacts only on contacts made on chromosomes that have not been excluded (inter).
     plot : bool, optional, default=True
@@ -118,6 +121,12 @@ def aggregate(
             df_contacts_1kb[unique_fragments].div(df_contacts_1kb[unique_fragments].sum(axis=0))
     else:
         norm_suffix = "absolute"
+
+    if additional_path:
+        df_additional: pd.DataFrame = pd.read_csv(additional_path, sep='\t')
+        probes_to_fragments = dict(zip(probes, fragments))
+        make_groups_of_probes(df_additional, df_contacts_10kb, probes_to_fragments)
+        make_groups_of_probes(df_additional, df_contacts_1kb, probes_to_fragments)
 
     if on == "centromeres":
         df_merged: pd.DataFrame = pd.merge(df_contacts_10kb, df_centros, on='chr')

sshic/ponder.py
@@ -1,10 +1,9 @@
 import re
 import os
-import sys
-import argparse
 import pandas as pd
 import numpy as np
 from typing import Optional
+from utils import make_groups_of_probes
 
 
 def ponder_mutant(
@@ -49,42 +48,11 @@ def ponder_mutant(
         df_frequencies.loc[:, frag] = df_frequencies.loc[:, frag] * ponder_coefficient
 
     if additional_path:
-        probe_to_frag = dict(zip(probes, fragments))
         df_additional: pd.DataFrame = pd.read_csv(additional_path, sep='\t')
-        for index, row in df_additional.iterrows():
-            group_probes = row["probes"].split(",")
-            group_frags = pd.unique([probe_to_frag[probe] for probe in group_probes])
-            group_name = row["name"]
-            if row["action"] == "average":
-                df_contacts[group_name] = df_contacts[group_frags].mean(axis=1)
-            elif row["action"] == "sum":
-                df_contacts[group_name] = df_contacts[group_frags].sum(axis=1)
-            else:
-                continue
-            df_frequencies[group_name] = df_contacts[group_name].div(sum(df_contacts[group_name]))
+        probes_to_fragments = dict(zip(probes, fragments))
+        make_groups_of_probes(df_additional, df_contacts, probes_to_fragments)
+        make_groups_of_probes(df_additional, df_frequencies, probes_to_fragments)
 
     df_contacts.to_csv(output_path+f"_{binned_type}_pondered_contacts.tsv", sep='\t', index=False)
     df_frequencies.to_csv(output_path + f"_{binned_type}_pondered_frequencies.tsv", sep='\t', index=False)
-
-
-def main(argv):
-    if not argv:
-        print('Please enter arguments correctly')
-        exit(0)
-
-    parser = argparse.ArgumentParser(
-        description='Ponder a mutant sample by an efficiency coefficient over the wt')
-    parser.add_argument('-s', '--sample-dir', type=str, required=True,
-                        help='Path to the sample_directory (containing binned and unbinned contacts/frequencies tsv)')
-
-    parser.add_argument('-b', '--binning', type=int, required=True, help='Binning size (in bp)')
-
-    parser.add_argument('-a', '--additional', type=str, required=False,
-                        help='Path to additional groups of probes table')
-
-    args = parser.parse_args(argv)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
 