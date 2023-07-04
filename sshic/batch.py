7e3ad9ef9103f4af4ed192db1ec3204ad25190f9
nmendiboure <nicolas.mendiboure@insa-lyon.fr>
2023-07-04 17:59:09 +0200

    minor refacto

--- sshic/batch.py
+++ sshic/batch.py
@@ -5,23 +5,6 @@ import numpy as np
 import pandas as pd
 
 
-base_dir = "/home/nicolas/Documents/Projects/ssHiC"
-script = join(base_dir, "hic_ssdna", "sshic", "pipeline.py")
-data_dir = join(base_dir, "data", "samples")
-inputs_dir = join(data_dir,  "inputs")
-refs_dir = join(inputs_dir, "refs")
-
-
-fragments = join(inputs_dir,  "fragments_list_S288c_DSB_LY_Capture_artificial_DpnIIHinfI.txt")
-oligos = join(inputs_dir, "capture_oligo_positions.csv")
-additional = join(inputs_dir, "additional_probe_groups.tsv")
-centromeres = join(inputs_dir, "S288c_chr_centro_coordinates.tsv")
-binning = ["1000", "2000", "3000", "5000", "10000", "20000", "40000", "50000", "80000", "100000"]
-ws_centros = 150000
-ws_telos = 150000
-excluded_chr = ["chr2", "chr3", "2_micron", "mitochondrion", "chr_artificial"]
-
-
 def run_pipeline(sample, reference):
     subprocess.run([
         "python3", script,
@@ -39,7 +22,23 @@ def run_pipeline(sample, reference):
         check=True)
 
 
-for pcr_type in ["PCRfree", "PCRdupkept"]:
+if __name__ == "__main__":
+    base_dir = "/home/nicolas/Documents/Projects/ssHiC"
+    script = join(base_dir, "hic_ssdna", "sshic", "pipeline.py")
+    data_dir = join(base_dir, "data", "samples")
+    inputs_dir = join(data_dir,  "inputs")
+    refs_dir = join(inputs_dir, "refs")
+    pcr_type = "PCRfree"
+
+    fragments = join(inputs_dir,  "fragments_list_S288c_DSB_LY_Capture_artificial_DpnIIHinfI.txt")
+    oligos = join(inputs_dir, "capture_oligo_positions.csv")
+    additional = join(inputs_dir, "additional_probe_groups.tsv")
+    centromeres = join(inputs_dir, "S288c_chr_centro_coordinates.tsv")
+    binning = ["1000", "2000", "3000", "5000", "10000", "20000", "40000", "50000", "80000", "100000"]
+    ws_centros = 150000
+    ws_telos = 150000
+    excluded_chr = ["chr2", "chr3", "2_micron", "mitochondrion", "chr_artificial"]
+
     samples_dir = join(data_dir, pcr_type)
     df_samp2ref: pd.DataFrame = pd.read_csv(join(inputs_dir, f"sample_vs_ref_ponder_{pcr_type}.tsv"), sep="\t")
     for samp in os.listdir(samples_dir):
