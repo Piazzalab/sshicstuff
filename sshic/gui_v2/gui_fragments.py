df0ed256011a210158bdc0a9ca6e67193757527c
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2023-08-04 16:06:14 +0200

    some refacto

--- sshic/gui_v2/gui_fragments.py
+++ sshic/gui_v2/gui_fragments.py
@@ -2,55 +2,52 @@ import os
 import re
 import pandas as pd
 import dash
-from os.path import join
+from os.path import join, isfile
 from os import listdir
-from dash import html
-from dash import dcc
-from dash import callback
-from dash import dash_table
+from dash import html, dcc, callback, dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
-
 layout = dbc.Container([
-    dbc.Row([
+    dbc.Row(
         dbc.Col([
-            html.Div(id='sample-id-output-container',
-                     style={'margin-top': '20px', 'margin-bottom': '20px'}),
+            html.Div(id='sample-id-output', style={'margin-top': '20px', 'margin-bottom': '20px'}),
             html.Br(),
-            html.Label("Select the digested fragments list :"),
-            dcc.Dropdown(
-                id='fragments-list-selector',
-                multi=False,
-            ),
-        ], width=6, style={'margin-top': '0px', 'margin-bottom': '25px'}),
-    ]),
-    dbc.Row([
+            html.Label("Select the digested fragments list:"),
+            dcc.Dropdown(id='fragments-selector', multi=False),
+        ], width=6, style={'margin-top': '0px', 'margin-bottom': '25px'})),
+
+    dbc.Row(
         dbc.Col([
-            html.Label("Select oligos related information table :"),
-            dcc.Dropdown(
-                id='capture-oligo-selector',
-                multi=False,
-            ),
-        ], width=6, style={'margin-top': '0px', 'margin-bottom': '30px'}),
+            html.Label("Select oligos related information table:"),
+            dcc.Dropdown(id='oligo-selector', multi=False),
+        ], width=6, style={'margin-top': '0px', 'margin-bottom': '30px'})),
+
+    dbc.Row([
+        dbc.Col(dash_table.DataTable(id='fragments-table'), width=4),
+        dbc.Col(dash_table.DataTable(id='oligo-table'), width=4),
+        dbc.Col(dash_table.DataTable(id='fragments-contacts-table'), width=4),
     ]),
 ])
 
 
 @callback(
-    Output('sample-id-output-container', 'children'),
-    [Input('sample-id', 'data')])
-def update_sample_id_output(value):
+    Output('sample-id-output', 'children'),
+    Input('sample-id', 'data')
+)
+def display_sample_id(value):
     return f"You are working on sample {value}"
 
 
 @callback(
-    [Output('fragments-list-selector', 'options'),
-     Output('capture-oligo-selector', 'options')],
-    [Input('data-inputs-path', 'data')])
-def update_fragments_list_selector(inputs_dir_data):
-    if inputs_dir_data:
-        inputs_list = sorted([f for f in listdir(inputs_dir_data) if os.path.isfile(join(inputs_dir_data, f))])
-        return [{'label': f, 'value': f} for f in inputs_list], [{'label': f, 'value': f} for f in inputs_list]
+    [Output('fragments-selector', 'options'),
+     Output('oligo-selector', 'options')],
+    Input('data-inputs-path', 'data')
+)
+def update_input_selectors(inputs_dir):
+    if inputs_dir:
+        input_files = sorted([file for file in listdir(inputs_dir) if isfile(join(inputs_dir, file))])
+        options = [{'label': file, 'value': file} for file in input_files]
+        return options, options
     return dash.no_update, dash.no_update
--- sshic/gui_v2/home.py
+++ sshic/gui_v2/home.py
@@ -2,70 +2,105 @@ import os
 import re
 import dash
 import shutil
-from os.path import join, dirname, isdir, isfile
+from os.path import join, dirname, isdir
 from dash import html, dcc
 from dash import callback
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 
-layout = html.Div([
-    dbc.Container([
-        dbc.Row(
-            dbc.Col(
-                html.H1('Welcome to our single strand Hi-C (sshic) platform'),
-                width={'size': 6, 'offset': 3},
-                style={'text-align': 'center', 'margin-top': '50px', 'margin-bottom': '50px'}
-            )
-        ),
-        dbc.Row(
-            dbc.Col([
-                html.Label('Please specify the location of your data (absolute path) :',
-                           style={'margin-top': '10px', 'margin-bottom': '10px'}),
-                dcc.Input(
-                    id='data-dir-input',
-                    type='text',
-                    placeholder='Input the folder path here',
-                    value=join(dirname(dirname(os.getcwd())), "data"),
-                    style={'width': '100%'}
-                ),
-            ], width=6, style={'margin-top': '50px', 'margin-bottom': '50px'}),
-        ),
-        dbc.Row([
-            dbc.Col([
-                html.Label("Select a PCR duplicates filter folder :"),
-                dcc.Dropdown(
-                    id='pcr-selector',
-                    multi=False,
-                ),
-                html.Br(),
-            ]),
+def create_layout():
+    return html.Div([
+        dbc.Container([
+            create_header(),
+            create_data_dir_row(),
+            create_pcr_selector_row(),
+            create_sample_selector_row(),
+            create_reference_selector_row()
         ]),
-        dbc.Row([
-            dbc.Col([
-                html.Label("Select a Sample :"),
-                dcc.Dropdown(
-                    id='sample-file-selector',
-                    options=[],
-                    multi=False,
-                ),
-                html.Br(),
-            ]),
+    ])
+
+
+def create_header():
+    return dbc.Row(
+        dbc.Col(
+            html.H1('Welcome to our single strand Hi-C (sshic) platform'),
+            width={'size': 6, 'offset': 3},
+            style={'text-align': 'center', 'margin-top': '50px', 'margin-bottom': '50px'}
+        )
+    )
+
+
+def create_data_dir_row():
+    return dbc.Row(
+        dbc.Col([
+            html.Label('Please specify the location of your data (absolute path):',
+                       style={'margin-top': '10px', 'margin-bottom': '10px'}),
+            dcc.Input(
+                id='data-dir-input',
+                type='text',
+                placeholder='Input the folder path here',
+                value=join(dirname(dirname(os.getcwd())), "data"),
+                style={'width': '100%'}
+            ),
+        ], width=6, style={'margin-top': '50px', 'margin-bottom': '50px'}),
+    )
+
+
+def create_pcr_selector_row():
+    return dbc.Row([
+        dbc.Col([
+            html.Label("Select a PCR duplicates filter folder:"),
+            dcc.Dropdown(
+                id='pcr-selector',
+                multi=False,
+            ),
+            html.Br(),
         ]),
-        dbc.Row([
-            dbc.Col([
-                html.Label("Please indicate a WT reference if you wish to weight your contacts :"),
-                dcc.Dropdown(
-                    id='reference-selector',
-                    options=[],
-                    value=None,
-                    multi=False,
-                ),
-                html.Br(),
-            ]),
+    ])
+
+
+def create_sample_selector_row():
+    return dbc.Row([
+        dbc.Col([
+            html.Label("Select a Sample:"),
+            dcc.Dropdown(
+                id='sample-file-selector',
+                options=[],
+                multi=False,
+            ),
+            html.Br(),
         ]),
-    ]),
-])
+    ])
+
+
+def create_reference_selector_row():
+    return dbc.Row([
+        dbc.Col([
+            html.Label("Please indicate a WT reference if you wish to weight your contacts:"),
+            dcc.Dropdown(
+                id='reference-selector',
+                options=[],
+                value=None,
+                multi=False,
+            ),
+            html.Br(),
+        ]),
+    ])
+
+
+def get_files_from_dir(directory, filter_string=''):
+    return [
+        f for f in os.listdir(directory)
+        if isdir(join(directory, f)) and filter_string in f
+    ]
+
+
+def update_dropdown_options(directory_data, filter_string=''):
+    if directory_data:
+        dir_list = get_files_from_dir(directory_data, filter_string)
+        return [{'label': s, 'value': s} for s in dir_list]
+    return dash.no_update
 
 
 @callback(
@@ -73,11 +108,7 @@ layout = html.Div([
     Input('data-samples-path', 'data')
 )
 def update_pcr_filter_selector(samples_dir_data):
-    if samples_dir_data:
-        pcr_filters_dir_list = [pcr for pcr in os.listdir(samples_dir_data)
-                                if isdir(join(samples_dir_data, pcr)) and 'pcr' in pcr]
-        return [{'label': s, 'value': s} for s in pcr_filters_dir_list]
-    return dash.no_update
+    return update_dropdown_options(samples_dir_data, 'pcr')
 
 
 @callback(
@@ -88,11 +119,8 @@ def update_pcr_filter_selector(samples_dir_data):
 def update_sample_selector(samples_dir_data, pcr_value):
     if samples_dir_data and pcr_value:
         samples_dir = join(samples_dir_data, pcr_value)
-        samples = sorted([
-            s for s in os.listdir(samples_dir) if isfile(join(samples_dir, s))
-        ],
-            key=lambda x: int(re.search(r'AD(\d+)', x).group(1))
-        )
+        samples = sorted(get_files_from_dir(samples_dir),
+                         key=lambda x: int(re.search(r'AD(\d+)', x).group(1)))
         return [{'label': s, 'value': s} for s in samples]
     return dash.no_update
 
@@ -105,9 +133,7 @@ def update_sample_selector(samples_dir_data, pcr_value):
 def update_reference_selector(inputs_dir_data, sample_file_value):
     if inputs_dir_data and sample_file_value:
         refs_dir = join(inputs_dir_data, "references")
-        references = sorted([
-            r for r in os.listdir(refs_dir) if isfile(join(refs_dir, r))
-        ])
+        references = sorted(get_files_from_dir(refs_dir))
         return [{'label': r, 'value': r} for r in references]
     return dash.no_update
 
@@ -178,3 +204,5 @@ def create_samp_dir(sample_name, sample_path_value, reference_path):
         return samp_dir
     return dash.no_update
 
+
+layout = create_layout()
