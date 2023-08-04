76ee6f2c3e57b8ca5f9051f8c783efb86380b472
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2023-08-04 14:42:46 +0200

    better spacing and added callback to update fragmentlist and oligo captuer selectors

--- sshic/gui_v2/gui_fragments.py
+++ sshic/gui_v2/gui_fragments.py
@@ -1,3 +1,4 @@
+import os
 import re
 import pandas as pd
 import dash
@@ -6,6 +7,7 @@ from os import listdir
 from dash import html
 from dash import dcc
 from dash import callback
+from dash import dash_table
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
@@ -14,21 +16,24 @@ import plotly.graph_objs as go
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.Div(id='sample-id-output-container'),
+            html.Div(id='sample-id-output-container',
+                     style={'margin-top': '20px', 'margin-bottom': '20px'}),
             html.Br(),
-            html.Label("Select the digested fragments list :",
-                       style={'margin-top': '20px', 'margin-bottom': '20px'}),
+            html.Label("Select the digested fragments list :"),
             dcc.Dropdown(
                 id='fragments-list-selector',
                 multi=False,
             ),
-            html.Label("Select oligos related information table :",
-                       style={'margin-top': '20px', 'margin-bottom': '20px'}),
+        ], width=6, style={'margin-top': '0px', 'margin-bottom': '25px'}),
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.Label("Select oligos related information table :"),
             dcc.Dropdown(
                 id='capture-oligo-selector',
                 multi=False,
             ),
-        ], width=4, style={'position': 'absolute', 'top': '100px', 'left': '25px'}),
+        ], width=6, style={'margin-top': '0px', 'margin-bottom': '30px'}),
     ]),
 ])
 
@@ -36,5 +41,16 @@ layout = dbc.Container([
 @callback(
     Output('sample-id-output-container', 'children'),
     [Input('sample-id', 'data')])
-def update_output(value):
+def update_sample_id_output(value):
     return f"You are working on sample {value}"
+
+
+@callback(
+    [Output('fragments-list-selector', 'options'),
+     Output('capture-oligo-selector', 'options')],
+    [Input('data-inputs-path', 'data')])
+def update_fragments_list_selector(inputs_dir_data):
+    if inputs_dir_data:
+        inputs_list = sorted([f for f in listdir(inputs_dir_data) if os.path.isfile(join(inputs_dir_data, f))])
+        return [{'label': f, 'value': f} for f in inputs_list], [{'label': f, 'value': f} for f in inputs_list]
+    return dash.no_update, dash.no_update
