8247c0506a34888b55d0db6aefa470585ec8a578
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2023-08-08 16:54:43 +0200

    minor changes

--- sshic/gui_v2/file_tree.py
+++ sshic/gui_v2/file_tree.py
@@ -5,6 +5,7 @@ import dash_mantine_components as dmc
 #   source :
 #   https://community.plotly.com/t/file-explorer-tree-generator-for-local-files/68732
 
+
 class FileTree:
 
     def __init__(self, filepath: os.PathLike):
@@ -18,22 +19,24 @@ class FileTree:
             self.build_tree(self.filepath, isRoot=True),
             multiple=True)
 
+    @staticmethod
     def flatten(self, l):
         return [item for sublist in l for item in sublist]
 
+    @staticmethod
     def make_file(self, file_name):
         return dmc.Text(
             [DashIconify(icon="akar-icons:file"), " ", file_name], style={"paddingTop": '5px'}
         )
 
+    @staticmethod
     def make_folder(self, folder_name):
         return [DashIconify(icon="akar-icons:folder"), " ", folder_name]
 
     def build_tree(self, path, isRoot=False):
         d = []
         if os.path.isdir(path):
-            children = self.flatten([self.build_tree(os.path.join(path, x))
-                                    for x in os.listdir(path)])
+            children = self.flatten([self.build_tree(os.path.join(path, x)) for x in os.listdir(path)])
             if isRoot:
                 d.append(
                     dmc.AccordionItem(
