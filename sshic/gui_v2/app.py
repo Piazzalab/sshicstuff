eeaa778c65383309424173d163b8fd596ddd98ac
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2023-08-21 17:16:33 +0200

    removed samples_viewer

--- sshic/gui_v2/app.py
+++ sshic/gui_v2/app.py
@@ -9,7 +9,6 @@ import home
 import data_viewer
 import gui_pipeline
 import probes_viewer
-import samples_viewer
 
 # Create a Dash application instance:
 server = Flask(__name__, template_folder='templates', )
@@ -30,9 +29,7 @@ app.layout = html.Div([
                  dcc.Tab(label='Pipeline', value='pipeline',
                          className='custom-tab', selected_className='custom-tab-selected'),
                  dcc.Tab(label='Probes Viewer', value='probes-viewer',
-                         className='custom-tab', selected_className='custom-tab-selected'),
-                 dcc.Tab(label='Samples Viewer', value='samples-viewer',
-                         className='custom-tab', selected_className='custom-tab-selected'),
+                         className='custom-tab', selected_className='custom-tab-selected')
              ]),
     html.Div(id='page-content'),
     dcc.Store(id='data-basedir'),
@@ -53,8 +50,6 @@ def display_page(value):
         return gui_pipeline.layout
     elif value == 'probes-viewer':
         return probes_viewer.layout
-    elif value == 'samples-viewer':
-        return samples_viewer.layout
 
 
 if __name__ == '__main__':
--- sshic/gui_v2/probes_viewer.py
+++ sshic/gui_v2/probes_viewer.py
@@ -7,164 +7,293 @@ from dash import html
 from dash import dcc
 from dash import callback
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, ALL
 import plotly.graph_objs as go
 
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("Probe #1"),
-                dbc.CardBody([
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select PCR mode:",
-                                       style={'margin-top': '0px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-pcr-selector-1',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select sample:",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-sample-selector-1',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select weight :",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-weight-selector-1',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select probes :",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-probe-selector-1',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-                ]),
-            ]),
-        ], width=6),
-
-        dbc.Col([
-            dbc.Card([
-                dbc.CardHeader("Probe #2"),
-                dbc.CardBody([
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select PCR mode:",
-                                       style={'margin-top': '0px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-pcr-selector-2',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select sample:",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-sample-selector-2',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select weight :",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-weight-selector-2',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-
-                    dbc.Row([
-                        dbc.Col([
-                            html.Label("Select probes :",
-                                       style={'margin-top': '10px', 'margin-bottom': '5px'}),
-                            dcc.Dropdown(
-                                id='pv-probe-selector-2',
-                                multi=False,
-                            )
-                        ]),
-                    ]),
-                ]),
-            ]),
-        ], width=6),
-    ], style={'margin-top': '20px', 'margin-bottom': '10px'}),
-
-
-
-    dbc.Row([
-        dbc.Col([
-            html.Label("Select binning :", style={'margin-top': '20px', 'margin-bottom': '20px'}),
-            dcc.Slider(
-                id='pv-binning-slider',
-                min=1,
-                max=100,
-                step=1,
-                value=10,
-                marks={1: "1"} | {i: str(i) for i in range(10, 101, 10)},
-                included=False
-            )
-        ], width=6),
+            html.Button(id="pv-add-card-button", className="blue-button", children="Add probe"),
+            dbc.Tooltip("Click to select another probe to display",
+                        target="pv-add-card-button", className="custom-tooltip", placement="right"),
+        ], width=2, className="ml-auto mt-2"),
     ]),
-
-    dbc.Row([
-        dbc.Col([
-            html.Div(id='pv-slider-output-container'),
-            html.Br(),
-        ], width=4, style={'margin-top': '10px', 'margin-bottom': '0px', 'margin-left': '20px'}),
-    ]),
-
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='pv-graph1',
-                      config={'displayModeBar': True, 'scrollZoom': True},
-                      style={'height': 'auto', 'width': '100%'}),
-        ], width=12, align='center'),
-    ], style={'margin-top': '300px', 'margin-left': '0px'}),
-
-    dbc.Row([
-        dbc.Col([
-            dcc.Graph(id='pv-graph2',
-                      config={'displayModeBar': True, 'scrollZoom': True},
-                      style={'height': 'auto', 'width': '100%'}),
-        ], width=12, align='center'),
-    ], style={'margin-top': '100px', 'margin-left': '0px'})
+    html.Div(id='pv-dynamic-probes-cards', children=[], style={'margin-top': '20px', 'margin-bottom': '20px'})
 ])
 
 
 @callback(
-    Output('pv-slider-output-container', 'children'),
-    [Input('pv-binning-slider', 'value')])
-def update_output(value):
-    return 'You have selected a binning of {} kb'.format(value)
+    Output('pv-dynamic-probes-cards', 'children'),
+    Output('pv-add-card-button', 'n_clicks'),
+    Input('pv-add-card-button', 'n_clicks')
+)
+def update_probes_cards(n_clicks):
+    if n_clicks is None:
+        return [], dash.no_update
+
+    probes_cards = []
+    for i in range(n_clicks):
+        sample_card = dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(html.Div(id={'type': 'probe-card-header', 'index': i})),
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Dropdown(
+                                options=[],
+                                value=None,
+                                placeholder="Select sample",
+                                id={'type': 'probe-dropdown', 'index': i},
+                                multi=False,
+                            )
+                        ]),
+
+                        dbc.Col([
+                            dcc.Dropdown(
+                                options=[],
+                                value=None,
+                                placeholder="Select probe",
+                                id={'type': 'probe-dropdown', 'index': i},
+                                multi=False,
+                            )
+                        ]),
+                    ]),
+
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Checklist(
+                                options=[
+                                    {'label': 'pcrfree', 'value': 'pcrfree'},
+                                    {'label': 'pcrdupkept', 'value': 'pcrdupkept'}
+                                ],
+                                value=[],
+                                id={'type': 'pcr-checkboxes', 'index': i},
+                                inline=True,
+                                className='custom-checkbox-label',
+                                labelStyle={"margin": "5px"}
+                            )
+                        ]),
+
+                        dbc.Col([
+                            dcc.Checklist(
+                                options=[
+                                    {'label': 'weighted', 'value': 'weighted'},
+                                    {'label': 'not_weighted', 'value': 'not_weighted'}
+                                ],
+                                value=[],
+                                id={'type': 'weight-checkboxes', 'index': i},
+                                inline=True,
+                                className='custom-checkbox-label',
+                                labelStyle={"margin": "5px"}
+                            )
+                        ]),
+                    ]),
+                ])
+            ])
+        )
+
+        probes_cards.append(sample_card)
+
+    rows = []
+    for i in range(0, len(probes_cards), 3):
+        row = dbc.Row(probes_cards[i:i+3], style={'margin-top': '20px', 'margin-bottom': '20px'})
+        rows.append(row)
+
+    return rows, n_clicks
 
 
+@callback(
+    Output({'type': 'pcr-checkboxes', 'index': ALL}, 'value'),
+    Input({'type': 'pcr-checkboxes', 'index': ALL}, 'value'),
+)
+def update_pcr_checkboxes(pcr_values):
+    for i in range(len(pcr_values)):
+        if not pcr_values[i]:
+            continue
+        if pcr_values[i][-1] == 'pcrfree':
+            pcr_values[i] = ['pcrfree']
+        elif pcr_values[i][-1] == 'pcrdupkept':
+            pcr_values[i] = ['pcrdupkept']
+        else:
+            pcr_values[i] = []
+    return pcr_values
+
+
+@callback(
+    Output({'type': 'weight-checkboxes', 'index': ALL}, 'value'),
+    Input({'type': 'weight-checkboxes', 'index': ALL}, 'value')
+)
+def update_weight_checkboxes(weight_values):
+    for i in range(len(weight_values)):
+        if not weight_values[i]:
+            continue
+        if weight_values[i][-1] == 'weighted':
+            weight_values[i] = ['weighted']
+        elif weight_values[i][-1] == 'not_weighted':
+            weight_values[i] = ['not_weighted']
+        else:
+            weight_values[i] = []
+    return weight_values
+
+
+
+
+
+# layout = dbc.Container([
+#     dbc.Row([
+#         dbc.Col([
+#             dbc.Card([
+#                 dbc.CardHeader("Probe #1"),
+#                 dbc.CardBody([
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select PCR mode:",
+#                                        style={'margin-top': '0px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-pcr-selector-1',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select sample:",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-sample-selector-1',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select weight :",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-weight-selector-1',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select probes :",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-probe-selector-1',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#                 ]),
+#             ]),
+#         ], width=6),
+#
+#         dbc.Col([
+#             dbc.Card([
+#                 dbc.CardHeader("Probe #2"),
+#                 dbc.CardBody([
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select PCR mode:",
+#                                        style={'margin-top': '0px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-pcr-selector-2',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select sample:",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-sample-selector-2',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select weight :",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-weight-selector-2',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#
+#                     dbc.Row([
+#                         dbc.Col([
+#                             html.Label("Select probes :",
+#                                        style={'margin-top': '10px', 'margin-bottom': '5px'}),
+#                             dcc.Dropdown(
+#                                 id='pv-probe-selector-2',
+#                                 multi=False,
+#                             )
+#                         ]),
+#                     ]),
+#                 ]),
+#             ]),
+#         ], width=6),
+#     ], style={'margin-top': '20px', 'margin-bottom': '10px'}),
+#
+#
+#
+#     dbc.Row([
+#         dbc.Col([
+#             html.Label("Select binning :", style={'margin-top': '20px', 'margin-bottom': '20px'}),
+#             dcc.Slider(
+#                 id='pv-binning-slider',
+#                 min=1,
+#                 max=100,
+#                 step=1,
+#                 value=10,
+#                 marks={1: "1"} | {i: str(i) for i in range(10, 101, 10)},
+#                 included=False
+#             )
+#         ], width=6),
+#     ]),
+#
+#     dbc.Row([
+#         dbc.Col([
+#             html.Div(id='pv-slider-output-container'),
+#             html.Br(),
+#         ], width=4, style={'margin-top': '10px', 'margin-bottom': '0px', 'margin-left': '20px'}),
+#     ]),
+#
+#     dbc.Row([
+#         dbc.Col([
+#             dcc.Graph(id='pv-graph1',
+#                       config={'displayModeBar': True, 'scrollZoom': True},
+#                       style={'height': 'auto', 'width': '100%'}),
+#         ], width=12, align='center'),
+#     ], style={'margin-top': '300px', 'margin-left': '0px'}),
+#
+#     dbc.Row([
+#         dbc.Col([
+#             dcc.Graph(id='pv-graph2',
+#                       config={'displayModeBar': True, 'scrollZoom': True},
+#                       style={'height': 'auto', 'width': '100%'}),
+#         ], width=12, align='center'),
+#     ], style={'margin-top': '100px', 'margin-left': '0px'})
+# ])
+#
+#
+# @callback(
+#     Output('pv-slider-output-container', 'children'),
+#     [Input('pv-binning-slider', 'value')])
+# def update_output(value):
+#     return 'You have selected a binning of {} kb'.format(value)
+#
+#
 # @callback(
 #     Output('pv-probe-selector', 'options'),
 #     Input('this-sample-out-dir-path', 'data')
