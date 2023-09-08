0afea620e8b7372c4fabe44c4fd196938360e4b6
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2023-09-08 16:23:14 +0200

    changed opacity

--- sshic/gui/probes_viewer.py
+++ sshic/gui/probes_viewer.py
@@ -16,30 +16,30 @@ from common import generate_data_table, prepare_dataframe_for_output
 import core.utils
 
 colors = [
-    'rgba(0, 0, 255, 0.2)',  # blue
-    'rgba(255, 0, 0, 0.2)',  # red
-    'rgba(249, 172, 37, 0.2)',  # yellow
-    'rgba(245, 0, 87, 0.2)',  # pink
-    'rgba(29, 233, 182, 0.2)',  # green
-    'rgba(255, 234, 0, 0.2)',  # yellow 2
-    'rgba(255, 11, 0, 0.2)',  # orange
-    'rgba(141, 110, 99, 0.2)',  # brown
-    'rgba(255, 64, 129, 0.2)',  # pink 2
-    'rgba(120, 144, 156, 0.2)',  # blue grey
-    'rgba(0, 131, 143, 0.2)',  # cyan
-    'rgba(171, 71, 188, 0.2)',  # purple
-    'rgba(255, 152, 0, 0.2)',  # amber
-    'rgba(0, 150, 136, 0.2)',  # teal
-    'rgba(0, 184, 212, 0.2)',  # cyan 2
-    'rgba(0, 200, 83, 0.2)',  # green 2
-    'rgba(229, 115, 115, 0.2)',  # red 2
-    'rgba(255, 167, 38, 0.2)',  # orange 2
-    'rgba(61, 90, 254, 0.2)',  # indigo
-    'rgba(68, 138, 255, 0.2)',  # blue 2
-    'rgba(121, 134, 203, 0.2)',  # deep purple
-    'rgba(170, 102, 68, 0.2)',  # deep orange
-    'rgba(255, 171, 145, 0.2)',  # pink 3
-    'rgba(255, 209, 128, 0.2)'  # amber 2
+    'rgba(0, 0, 255, 0.8)',  # blue
+    'rgba(255, 0, 0, 0.8)',  # red
+    'rgba(249, 172, 37, 0.8)',  # yellow
+    'rgba(245, 0, 87, 0.8)',  # pink
+    'rgba(29, 233, 182, 0.8)',  # green
+    'rgba(255, 234, 0, 0.8)',  # yellow 2
+    'rgba(255, 11, 0, 0.8)',  # orange
+    'rgba(141, 110, 99, 0.8)',  # brown
+    'rgba(255, 64, 129, 0.8)',  # pink 2
+    'rgba(120, 144, 156, 0.8)',  # blue grey
+    'rgba(0, 131, 143, 0.8)',  # cyan
+    'rgba(171, 71, 188, 0.8)',  # purple
+    'rgba(255, 152, 0, 0.8)',  # amber
+    'rgba(0, 150, 136, 0.8)',  # teal
+    'rgba(0, 184, 212, 0.8)',  # cyan 2
+    'rgba(0, 200, 83, 0.8)',  # green 2
+    'rgba(229, 115, 115, 0.8)',  # red 2
+    'rgba(255, 167, 38, 0.8)',  # orange 2
+    'rgba(61, 90, 254, 0.8)',  # indigo
+    'rgba(68, 138, 255, 0.8)',  # blue 2
+    'rgba(121, 134, 203, 0.8)',  # deep purple
+    'rgba(170, 102, 68, 0.8)',  # deep orange
+    'rgba(255, 171, 145, 0.8)',  # pink 3
+    'rgba(255, 209, 128, 0.8)'  # amber 2
 ]
 
 chr_names = [f"chr{i}" for i in range(1, 17)] + ["2_micron", "mitochondrion", "chr_artificial"]
