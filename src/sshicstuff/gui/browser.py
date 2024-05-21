a9391a0273be478ecaa1941a57f2337f536513b5
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2024-05-21 17:10:01 +0200

    WIP : replace delimiter by colorbar. Still need improvement like zoom in etc ..

--- src/sshicstuff/gui/browser.py
+++ src/sshicstuff/gui/browser.py
@@ -1,218 +1,33 @@
 import re
 import os
-
 import numpy as np
 import pandas as pd
+
+# plotly
 import plotly.graph_objs as go
-from dash import html, dcc, callback
-import dash_bootstrap_components as dbc
-import dash_daq as daq
+from plotly.subplots import make_subplots
+
+# dash
+from dash import callback
 from dash.dependencies import Input, Output, State
 
 # common.py
 from sshicstuff.gui.common import TEMPORARY_DIRECTORY
 from sshicstuff.gui.common import colors_rgba
-from sshicstuff.gui.common import colors_hex
 from sshicstuff.gui.common import empty_figure
 from sshicstuff.gui.common import uploaded_files
 from sshicstuff.gui.common import save_file
 from sshicstuff.gui.common import rebin_live
+from sshicstuff.gui.common import transform_data
+from sshicstuff.gui.common import make_colorbar
+from sshicstuff.gui.common import chr_to_exclude
 
+# layout.py
+from sshicstuff.gui.layout import layout
 
 if not os.path.exists(TEMPORARY_DIRECTORY):
     os.makedirs(TEMPORARY_DIRECTORY)
     
-
-layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            html.H6('Upload files'),
-            dcc.Upload(
-                id="upload-files",
-                children=html.Div(
-                    ["Drag and drop or click to select a file to upload."]
-                ),
-                style={
-                    "width": "100%",
-                    "height": "80px",
-                    "lineHeight": "80px",
-                    "borderWidth": "2px",
-                    "borderStyle": "dashed",
-                    "borderRadius": "20px",
-                    "textAlign": "center",
-                },
-                multiple=True,
-            ),
-        ], width=5, style={'margin-top': '0px', 'margin-bottom': '25px'}),
-
-        dbc.Col([
-            html.Button(
-                id="clear-list",
-                className="btn btn-danger",
-                children="Clear list",
-            )
-        ], width=2, style={'margin-top': '50px', 'margin-bottom': '20px', 'margin-left': '20px'}),
-    ]),
-
-    dbc.Row([
-        # Input files dropdown COLUMN
-        dbc.Col([
-            html.H6('Input files dropdown'),
-            dbc.Row([
-                dcc.Dropdown(
-                    id='oligo-dropdown',
-                    placeholder="Select capture oligos file",
-                    multi=False),
-            ], style={'margin-top': '10px', 'margin-bottom': '20px'}),
-
-            dbc.Row([
-                dcc.Dropdown(
-                    id='coord-dropdown',
-                    placeholder="Select chr. coordinates file",
-                    multi=False),
-            ], style={'margin-top': '10px', 'margin-bottom': '20px'}),
-
-            dbc.Row([
-                dcc.Dropdown(
-                    id='samples-dropdown',
-                    placeholder="Select sample file",
-                    multi=False
-                ),
-            ], style={'margin-top': '10px', 'margin-bottom': '20px'}),
-
-            dbc.Row([
-                dcc.Dropdown(
-                    id='probes-dropdown',
-                    placeholder="Select probe(s) or group of probes",
-                    multi=True
-                ),
-            ], style={'margin-top': '10px', 'margin-bottom': '20px'}),
-        ], width=6),
-
-        # Region & binning settings COLUMN
-        dbc.Col([
-            dbc.Row([
-                html.Div(
-                    id='slider-output-container', style={'font-size': '14px', 'margin-bottom': '10px'}),
-                dcc.Slider(
-                    id='binning-slider',
-                    min=0,
-                    max=100,
-                    step=1,
-                    value=10,
-                    marks={i: str(i) for i in range(0, 101, 10)},
-                    included=False,
-                ),
-            ]),
-
-            dbc.Row([
-                dbc.Label("Region", style={'font-size': '14px', 'margin-top': '20px'}),
-            ]),
-
-            dbc.Row([
-                dbc.Col([
-                    dcc.Dropdown(
-                        id='region-dropdown',
-                        value=None,
-                        placeholder="Select chromosome",
-                        multi=False),
-
-                ], width=6),
-
-                dbc.Col([
-                    dcc.Input(
-                        id='start-pos',
-                        placeholder="Start",
-                        value=None,
-                        type='text',
-                        className="custom-input"
-                    ),
-                ], width=3),
-
-                dbc.Col([
-                    dcc.Input(
-                        id='end-pos',
-                        placeholder="End",
-                        value=None,
-                        className="custom-input"
-                    ),
-                ], width=3),
-
-            ]),
-        ], width=6),
-    ]),
-
-    dbc.Row([
-        dbc.Col([
-            html.Button(
-                id="plot-button", className="plot-button", children="Plot",
-                style={'margin-top': '25px'}),
-        ], width=2),
-
-        dbc.Col([
-            dbc.Label("Y min", style={'font-size': '14px', 'margin-top': '10px'}),
-            dcc.Input(
-                id='y-min', type='number', value=None,
-                placeholder='Y min', className="custom-input"),
-        ], width=1),
-
-        dbc.Col([
-            dbc.Label("Y max", style={'font-size': '14px', 'margin-top': '10px'}),
-            dcc.Input(
-                id='y-max', type='number', value=None,
-                placeholder='Y max', className="custom-input"),
-        ], width=1),
-
-        dbc.Col([
-            dbc.Label("Height", style={'font-size': '14px', 'margin-top': '10px'}),
-            dcc.Input(
-                id='height', type='number', value=600, step=20,
-                placeholder='Height', className="custom-input"),
-        ], width=1),
-
-        dbc.Col([
-            dbc.Label("Width", style={'font-size': '14px', 'margin-top': '10px'}),
-            dcc.Input(
-                id='width', type='number', value=1500, step=20,
-                placeholder='Width', className="custom-input"),
-        ], width=1),
-
-        dbc.Col([
-            daq.BooleanSwitch(
-                id='re-scale-switch',
-                on=False,
-                label='Re-scale'
-            ),
-            html.Div(id='re-scale-output',
-                     style={'margin-top': '10px', 'margin-left': '30px', 'font-size': '12px'}),
-
-        ], width=1, style={'margin-top': '10px', 'margin-bottom': '10px', 'margin-left': '20px'}),
-
-        dbc.Col([
-            daq.BooleanSwitch(
-                id='delimiter-switch',
-                on=False,
-                label='Delimiter'
-            ),
-        ], width=1, style={'margin-top': '10px', 'margin-bottom': '10px', 'margin-left': '20px'}),
-    ]),
-
-    dbc.Row([
-        # html.Div(id='graphs', children=[], style={'margin-top': '20px', 'margin-bottom': '20px'}),
-        dcc.Graph(
-            id='graph',
-            config={
-                'displayModeBar': True,
-                'scrollZoom': True,
-                'doubleClick': 'reset',
-                'autosizable': False
-            },
-            style={'height': '100%', 'width': '100%'},
-            figure=empty_figure
-        )
-    ])
-])
-
 
 @callback(
     Output('slider-output-container', 'children'),
@@ -266,6 +81,7 @@ def update_region_dropdown(coord_value):
         return []
 
     df = pd.read_csv(coord_value, sep='\t')
+    df = df[~df['chr'].isin(chr_to_exclude)]
     chr_list = df['chr'].unique()
     chr_list = [f"{c}" for c in chr_list]
 
@@ -313,7 +129,6 @@ def update_probes_dropdown(oligo_value, sample_value):
      State('y-min', 'value'),
      State('y-max', 'value'),
      State('re-scale-switch', 'on'),
-     State('delimiter-switch', 'on'),
      State('height', 'value'),
      State('width', 'value'),]
 )
@@ -330,21 +145,19 @@ def update_graph(
         user_y_min,
         user_y_max,
         re_scale,
-        delimit,
         height,
         width
 ):
-
     re_scale_output = ""
+
     if n_clicks is None or n_clicks == 0:
         return empty_figure, re_scale_output
     if not samples_value or not probes_value:
         return empty_figure, re_scale_output
 
-    fig = go.Figure()
-
     # coordinates & genomic (cumulative) positions stuff
     df_coords = pd.read_csv(coords_value, sep='\t')
+    df_coords = df_coords[~df_coords['chr'].isin(chr_to_exclude)]
     df_chr_len = df_coords[["chr", "length"]]
     df_chr_len["chr_start"] = df_chr_len["length"].shift().fillna(0).astype("int64")
     df_chr_len["cumu_start"] = df_chr_len["chr_start"].cumsum()
@@ -353,6 +166,7 @@ def update_graph(
     sample_name = samples_value.split('/')[-1].split('.')[0]
 
     df = df_samples[["chr", "start", "sizes", "genome_start"] + probes_value]
+    df = df[~df['chr'].isin(chr_to_exclude)]
 
     if binning_value > 0:
         binning_value *= 1000  # kbp convert to bp
@@ -385,21 +199,14 @@ def update_graph(
     y_max = float(user_y_max) if user_y_max else df[probes_value].max().max()
 
     if re_scale:
-        data = df[probes_value].values
-        if y_max <= 1.:
-            # sqrt transformation
-            new_data = np.sqrt(data + 1e-8)
-            y_max = np.sqrt(y_max) if not user_y_max else user_y_max
-            y_min = np.sqrt(y_min) if y_min > 0 else 0
-            re_scale_output = "sqrt"
-        else:
-            # log transformation
-            new_data = np.log(data + 1)
-            y_max = np.log(y_max) if not user_y_max else user_y_max
-            y_min = 0
-            re_scale_output = "log"
-
+        data = df[probes_value]
+        new_data, y_max, y_min, re_scale_output = transform_data(data, y_max, user_y_max, y_min, re_scale)
         df[probes_value] = new_data
+
+    fig = make_subplots(
+        rows=2, cols=1, row_heights=[0.94, 0.06], vertical_spacing=0.06,
+        specs=[[{'type': 'scatter'}], [{'type': 'bar'}]]
+    )
 
     for j in range(len(probes_value)):
         frag = probes_value[j]
@@ -412,49 +219,44 @@ def update_graph(
                 mode='lines',
                 line=dict(width=1, color=colors_rgba[trace_id]),
                 marker=dict(size=4)
-            )
+            ),
+            row=1, col=1
         )
 
+    color_bar, chr_tick_pos = make_colorbar(df_coords, binning_value)
+    unique_chr_list = df_coords['chr'].unique()
+
+    fig.add_trace(color_bar, row=2, col=1)
     fig.update_layout(
-        width=width,
-        height=height,
         title=f"{sample_name}",
-        xaxis=dict(domain=[0.0, 0.9], title=x_label),
-        yaxis=dict(title="Contact frequency"),
+        xaxis=dict(
+            # title=x_label
+        ),
+        xaxis2=dict(
+            tickmode='array',
+            tickvals=chr_tick_pos,
+            ticktext=unique_chr_list,
+            tickfont=dict(size=12),
+        ),
+        yaxis=dict(
+            title="Contact frequency"
+        ),
+        yaxis2=dict(
+            showticklabels=False,
+        ),
+
+        xaxis_showgrid=False,
+        yaxis_showgrid=False,
         xaxis_type='linear',
         xaxis_tickformat="d",
         xaxis_range=[x_min, x_max],
         yaxis_range=[y_min, y_max],
         hovermode='closest',
         plot_bgcolor='white',
-        paper_bgcolor='white'
+        paper_bgcolor='white',
+        width=width,
+        height=height,
     )
-
-    if delimit and not region_value:
-        for xi, x_pos in enumerate(df_chr_len["cumu_start"].to_list()):
-            name_pos = x_pos + 100
-            fig.add_shape(
-                type='line',
-                yref='paper',
-                xref='x',
-                x0=x_pos, x1=x_pos,
-                y0=0, y1=1,
-                line=dict(color='gray', width=1, dash='dot')
-            )
-
-            fig.add_annotation(
-                go.layout.Annotation(
-                    x=name_pos,
-                    y=1.07,
-                    yref="paper",
-                    text=df_chr_len.loc[xi, "chr"],
-                    showarrow=False,
-                    xanchor="center",
-                    font=dict(size=11, color=colors_hex[xi]),
-                    textangle=330
-                ),
-                xref="x"
-            )
 
     if relayout_data:
         if 'xaxis.range[0]' in relayout_data:
