401498f65e81fa83516d37261a3966991b79095f
nmendiboure <nicolas.mendiboure@ens-lyon.fr>
2024-08-05 11:24:42 +0200

    minor update

--- setup.py
+++ setup.py
@@ -53,5 +53,9 @@ setuptools.setup(
     install_requires=REQUIREMENTS,
     include_package_data=True,
     zip_safe=False,
-    entry_points={"console_scripts": ["sshicstuff=sshicstuff.main:main"]}
+    entry_points={
+        "console_scripts":
+            ["sshicstuff=sshicstuff.main:main"
+             ]
+    }
 )
